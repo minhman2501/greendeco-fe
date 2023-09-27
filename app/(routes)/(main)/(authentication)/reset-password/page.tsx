1fa7490f6de826707b4ca079e6e62da14eeaaaa6
minhman <phminhman2501@gmail.com>
2023-09-27 22:29:32 +0700

    feat: handle missing token

--- app/(routes)/(main)/(authentication)/reset-password/page.tsx
+++ app/(routes)/(main)/(authentication)/reset-password/page.tsx
@@ -1,7 +1,15 @@
+'use client'
 import Link from 'next/link'
 import ResetPasswordForm from './ResetPasswordForm'
+import { useSearchParams } from 'next/navigation'
 
-export default function RegisterPage() {
+export default function ResetPasswordPage() {
+	const searchParams = useSearchParams()
+	const token = searchParams.get('token')
+
+	if (token == null) {
+		return <MissingTokenMessage />
+	}
 	return (
 		<>
 			<div className='flex-col-start gap-common'>
@@ -12,9 +20,34 @@ export default function RegisterPage() {
 					</div>
 				</div>
 				<ResetPasswordForm />
-				<span className='text-center text-body-md'>
+				<span className=' text-center text-body-md'>
 					Don&apos;t have an account? <Link href={'/login'}>Sign Up Now!</Link>
 				</span>
+			</div>
+		</>
+	)
+}
+
+function MissingTokenMessage() {
+	return (
+		<>
+			<div className='flex-col-start gap-cozy'>
+				<div className='flex-col-start gap-[4px]'>
+					<h1>Can&apos;t Reset Your Password</h1>
+					<p className='text-body-md'>
+						Seems like you haven&apos;t sent your registered email to us.
+					</p>
+				</div>
+				<div className='flex-col-start gap-[4px] text-body-md'>
+					<p>
+						In order to reset your account password, you need to{' '}
+						<Link href={'/forgot-password'}>send your registered email</Link> to us.
+					</p>
+					<p>
+						Or if you don&apos;t have an account,{' '}
+						<Link href={'/login'}>Sign Up Now!</Link>
+					</p>
+				</div>
 			</div>
 		</>
 	)

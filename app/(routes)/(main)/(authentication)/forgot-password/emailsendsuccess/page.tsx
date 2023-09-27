9ec3a25f0586987ec738e230b61e7a61c8dd1c27
minhman <phminhman2501@gmail.com>
2023-09-27 23:45:17 +0700

    feat: implement send email sucess

---
+++ app/(routes)/(main)/(authentication)/forgot-password/emailsendsuccess/page.tsx
@@ -0,0 +1,14 @@
+export default function RegisterPage() {
+	return (
+		<>
+			<div>
+				<div className='flex-col-start gap-[4px]'>
+					<h1>Your email has been sent ✅</h1>
+					<p className='text-body-md'>
+						Please check your mailbox to receive our password reset link.
+					</p>
+				</div>
+			</div>
+		</>
+	)
+}

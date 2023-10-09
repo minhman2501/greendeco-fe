20d1b1f5d84eb5919cc5b9b9b14119104ff8e0ab
minhman <phminhman2501@gmail.com>
2023-10-09 10:44:55 +0700

    feat: getCookie hook && constant access token cookie name variable

--- app/_hooks/useCookie.ts
+++ app/_hooks/useCookie.ts
@@ -6,3 +6,9 @@ type CookieParams = {
 export function setCookie({ name, value }: CookieParams) {
 	document.cookie = name + '=' + value + ';path=/'
 }
+
+export function getCookie(cookieName: string) {
+	const value = '; ' + document.cookie
+	const parts = value.split('; ' + cookieName + '=')
+	if (parts.length === 2) return parts.pop().split(';').shift()
+}

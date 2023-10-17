c81ec37a54c4a7234c4493771822c71ff6aaba15
minhman <phminhman2501@gmail.com>
2023-10-17 15:13:03 +0700

    feat: implement setting tab list

---
+++ app/(routes)/(main)/(customer)/user/setting/SettingTabList.tsx
@@ -0,0 +1,62 @@
+'use client'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+import clsx from 'clsx'
+
+type SettingTab = {
+	label: string
+	path: string
+}
+
+const settingTabList: SettingTab[] = [
+	{
+		path: 'profile',
+		label: 'Profile',
+	},
+	{
+		path: 'changepassword',
+		label: 'Change Password',
+	},
+	{
+		path: 'address',
+		label: 'Shipping Address',
+	},
+]
+
+export default React.memo(function SettingTabList() {
+	return (
+		<div
+			className='
+            flex w-full items-center gap-x-compact rounded-[4px] bg-primary-625 p-compact
+            '
+		>
+			{settingTabList.map((tab) => (
+				<Tab
+					key={tab.label}
+					path={tab.path}
+					label={tab.label}
+				/>
+			))}
+		</div>
+	)
+})
+
+const Tab = React.memo(function Tab({ label, path }: SettingTab) {
+	const currentPath = usePathname()
+	console.log(path, 'render')
+
+	return (
+		<Link
+			href={`./${path}`}
+			className={clsx('flex w-full justify-center rounded-[4px]  p-compact font-bold', {
+				' bg-primary-625  text-neutral-gray-1 hover:font-semibold':
+					currentPath.includes(path) === false,
+				'pointer-events-none cursor-auto cursor-pointer bg-white text-primary-625':
+					currentPath.includes(path),
+			})}
+		>
+			<label className='cursor-pointer text-body-sm uppercase'>{label}</label>
+		</Link>
+	)
+})
--- app/(routes)/(main)/(customer)/user/setting/layout.tsx
+++ app/(routes)/(main)/(customer)/user/setting/layout.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import SettingTabList from './SettingTabList'
 
 export default function UserSettingLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<div className='flex-col-start gap-cozy rounded-[8px] bg-white p-comfortable shadow-30'>
-			<nav>Setting Nav</nav>
+			<SettingTabList />
 			{children}
 		</div>
 	)

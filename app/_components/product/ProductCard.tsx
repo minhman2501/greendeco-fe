85bfe96348b405433c33e0c688e28a3f740a0e47
minhman <phminhman2501@gmail.com>
2023-10-19 21:31:53 +0700

    feat: implement product card props

--- app/_components/product/ProductCard.tsx
+++ app/_components/product/ProductCard.tsx
@@ -2,26 +2,44 @@ import Image from 'next/image'
 import { StarIcon } from '@heroicons/react/24/solid'
 import { ShoppingBagIcon } from '@heroicons/react/24/outline'
 
-export default function ProductCard() {
+export type ProductCardProps = {
+	id: string
+	name: string
+	image: string
+	review?: string[]
+	price: string
+}
+
+export default function ProductCard(props: ProductCardProps) {
+	const { id, name, image, price } = props
 	return (
-		<span className='w-full overflow-hidden rounded-[16px] shadow-63'>
-			<CardImage />
+		<span className='w-full overflow-hidden rounded-[8px] shadow-63'>
+			<CardImage imageUrl={image} />
 			<div className='flex w-full p-cozy'>
-				<CardDetail />
+				<CardDetail
+					name={name}
+					price={price}
+				/>
 				<span className='flex items-center justify-center'>
-					<AddToCartButton />
+					<AddToCartButton productId={id} />
 				</span>
 			</div>
 		</span>
 	)
 }
 
-function CardDetail() {
+function CardDetail({
+	name,
+	price,
+}: {
+	name: ProductCardProps['name']
+	price: ProductCardProps['price']
+}) {
 	return (
 		<div className='flex-col-start h-full w-full justify-between gap-cozy'>
 			<div className='flex-col-start'>
 				<span className='cursor-pointer text-body-sm font-semibold text-primary-625 hover:underline'>
-					Cat Palm
+					{name}
 				</span>
 				<span className='flex items-center gap-[2px]'>
 					<StarIcon
@@ -31,16 +49,16 @@ function CardDetail() {
 					<span className='text-body-xsm text-primary-418-60'>4.7 (15 reviews)</span>
 				</span>
 			</div>
-			<div className='text-body-sm'>$ 35.99</div>
+			<div className='text-body-sm'>$ {price}</div>
 		</div>
 	)
 }
-function CardImage() {
+function CardImage({ imageUrl }: { imageUrl: ProductCardProps['image'] }) {
 	return (
 		<div className='h-[280px] w-full'>
 			<Image
-				src='https://bloomscape.com/cdn-cgi/image/quality=75,fit=scale-down,height=580,width=696,metadata=copyright,format=webp/wp-content/uploads/2023/08/230805_BB_fiscus_elastica_tineke_0876-e1692910966662.jpg?ver=1057285'
-				alt='product card'
+				src={imageUrl}
+				alt='product image'
 				width={0}
 				height={0}
 				sizes='100vw'
@@ -49,7 +67,7 @@ function CardImage() {
 	)
 }
 
-function AddToCartButton() {
+function AddToCartButton({ productId }: { productId: ProductCardProps['id'] }) {
 	return (
 		<span className='flex h-fit w-fit  items-center justify-center rounded-[100%] bg-primary-625 p-[12px]'>
 			<ShoppingBagIcon

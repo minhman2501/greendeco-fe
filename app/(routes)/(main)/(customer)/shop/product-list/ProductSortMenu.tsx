a242a0401d431e1d300d94480daf6193d987082b
minhman <phminhman2501@gmail.com>
2023-10-21 11:14:40 +0700

    feat: sort menu first version

---
+++ app/(routes)/(main)/(customer)/shop/product-list/ProductSortMenu.tsx
@@ -0,0 +1,70 @@
+'use client'
+
+import type { ChangeEvent } from 'react'
+import useQueryParams from '@/app/_hooks/useQueryParams'
+import { useCallback } from 'react'
+
+type SortOptionsType = {
+	sort?: 'asc' | 'desc'
+	sortBy?: string
+}
+const options: SortOptionsType[] = [
+	{
+		sort: 'asc',
+		sortBy: 'price',
+	},
+	{
+		sort: 'desc',
+		sortBy: 'price',
+	},
+	{
+		sort: 'asc',
+		sortBy: 'created_at',
+	},
+	{
+		sort: 'desc',
+		sortBy: 'created_at',
+	},
+]
+
+export const SortMenu = () => {
+	const { queryParams, setQueryParams } = useQueryParams<SortOptionsType>()
+
+	const urlQueryParams = new URLSearchParams(queryParams.toString())
+	const optionFilter = Object.fromEntries(urlQueryParams)
+
+	const onSelect = useCallback(
+		(event: ChangeEvent<HTMLSelectElement>) => {
+			if (event.target.value === JSON.stringify(optionFilter) || !event.target.value) {
+				setQueryParams({
+					sort: undefined,
+					sortBy: undefined,
+				})
+				return
+			}
+
+			setQueryParams(JSON.parse(event.target.value))
+		},
+		[setQueryParams, optionFilter],
+	)
+
+	return (
+		<select onChange={onSelect}>
+			{options.map((opt) => (
+				<option
+					key={JSON.stringify(opt)}
+					value={JSON.stringify(opt)}
+				>
+					{opt.sortBy === 'price' &&
+						(opt.sort === 'asc'
+							? 'Price increase'
+							: opt.sort === 'desc'
+							? 'Price decrease'
+							: '')}
+					{opt.sortBy === 'created_at' &&
+						(opt.sort === 'asc' ? 'Newest' : opt.sort === 'desc' ? 'Oldest' : '')}
+				</option>
+			))}
+		</select>
+	)
+}

ecc2ad555cbf1f88b928345ebcfbd7322b286f2d
minhman <phminhman2501@gmail.com>
2023-10-21 17:52:56 +0700

    feat: implement to test the components

--- app/(routes)/(main)/(customer)/shop/product-list/layout.tsx
+++ app/(routes)/(main)/(customer)/shop/product-list/layout.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import { SortMenu } from './ProductSortMenu'
+import FilterMenu from './ProductFilter'
 
 export default function ProductListLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<>
 			<div>Banner</div>
 			<div className='grid grid-cols-12 gap-comfortable'>
-				<div className='col-span-4'>Filter</div>
-				<div className='col-span-8'>{children}</div>
+				<div className='col-span-3'>
+					<SortMenu />
+					<FilterMenu />
+				</div>
+				<div className='col-span-9'>{children}</div>
 			</div>
 		</>
 	)

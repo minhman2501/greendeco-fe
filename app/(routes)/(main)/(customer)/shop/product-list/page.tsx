c5e3c62e45259b45d4b6c4a636312a98d175545d
minhman <phminhman2501@gmail.com>
2023-10-21 17:53:24 +0700

    test: testing the filter component

--- app/(routes)/(main)/(customer)/shop/product-list/page.tsx
+++ app/(routes)/(main)/(customer)/shop/product-list/page.tsx
@@ -1,3 +1,34 @@
+'use client'
+import { FilterParams, getProductList } from '@/app/_api/axios/product'
+import ProductCardsGrid from '@/app/_components/product/ProductGrid'
+import { useQuery } from '@tanstack/react-query'
+import useQueryParams from '@/app/_hooks/useQueryParams'
+
 export default function ProductListPage() {
-	return <h1>Product List</h1>
+	const { queryObject, queryParams } = useQueryParams<FilterParams>()
+
+	const field = queryParams?.get('field')
+	const result = field ? field : ''
+	result !== '' ? console.log('field', JSON.parse(result)) : null
+	const productListQuery = useQuery({
+		queryKey: ['product', field],
+		queryFn: () =>
+			getProductList({
+				field: JSON.parse(result),
+			}),
+	})
+
+	if (productListQuery.data)
+		return (
+			<>
+				<div className='flex items-center justify-between'>
+					<span>count {productListQuery.data.page_size}</span>
+				</div>
+				<ProductCardsGrid
+					productList={productListQuery.data.items}
+					columns={4}
+					gap='cozy'
+				/>
+			</>
+		)
 }

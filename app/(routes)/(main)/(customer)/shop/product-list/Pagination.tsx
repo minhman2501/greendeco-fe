3283aa25a3a744f8bb2fc33b25994fca19835f47
minhman <phminhman2501@gmail.com>
2023-10-22 10:16:28 +0700

    feat: initial fully functional pagination (without page number)

---
+++ app/(routes)/(main)/(customer)/shop/product-list/Pagination.tsx
@@ -0,0 +1,49 @@
+'use client'
+import { useCallback } from 'react'
+import useQueryParams from '@/app/_hooks/useQueryParams'
+import Button from '@/app/_components/Button'
+
+type PaginationProps = {
+	offSet: number
+	next: boolean
+	prev: boolean
+}
+
+type QueryPararms = Pick<PaginationProps, 'offSet'>
+
+export default function Pagination(props: PaginationProps) {
+	const { offSet, next, prev } = props
+
+	const { setQueryParams } = useQueryParams<QueryPararms>()
+	const currentPage = offSet
+
+	const changePage = useCallback(
+		(pageNumber: number) => {
+			if (pageNumber === currentPage || !pageNumber) setQueryParams({ offSet: undefined })
+
+			setQueryParams({ offSet: pageNumber })
+		},
+		[setQueryParams, currentPage],
+	)
+
+	const handlePageChange = (destinationPage: number) => {
+		changePage(destinationPage)
+	}
+
+	return (
+		<div className='flex items-center gap-comfortable'>
+			<Button
+				disabled={!prev}
+				onClick={() => handlePageChange(currentPage - 1)}
+			>
+				Previous Page
+			</Button>
+			<Button
+				disabled={!next}
+				onClick={() => handlePageChange(currentPage + 1)}
+			>
+				Next Page
+			</Button>
+		</div>
+	)
+}

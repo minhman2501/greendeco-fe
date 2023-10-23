a980cfae249e27804b7fcefe1e4753bd805a7a43
minhman <phminhman2501@gmail.com>
2023-10-23 23:15:39 +0700

    feat: add react skeleton && implement to product list

--- app/(routes)/(main)/(customer)/shop/product-list/loading.tsx
+++ app/(routes)/(main)/(customer)/shop/product-list/loading.tsx
@@ -1,3 +1,50 @@
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
 export default function ProductListLoading() {
-	return <h1>Loading product</h1>
+	return (
+		<div className='grid grid-cols-4 gap-cozy'>
+			<SkeletonTheme
+				baseColor='#e4e8e3'
+				highlightColor='#f7f8f7'
+				duration={2}
+				borderRadius={4}
+			>
+				<span>
+					<Skeleton
+						height={220}
+						className='mb-[8px]'
+					/>
+					<p>
+						<Skeleton count={2} />
+					</p>
+				</span>
+				<span>
+					<Skeleton
+						className='mb-[8px]'
+						height={220}
+					/>
+					<p>
+						<Skeleton count={2} />
+					</p>
+				</span>
+				<span>
+					<Skeleton
+						className='mb-[8px]'
+						height={220}
+					/>
+					<p>
+						<Skeleton count={2} />
+					</p>
+				</span>
+				<span>
+					<Skeleton
+						className='mb-[8px]'
+						height={220}
+					/>
+					<p>
+						<Skeleton count={2} />
+					</p>
+				</span>
+			</SkeletonTheme>
+		</div>
+	)
 }

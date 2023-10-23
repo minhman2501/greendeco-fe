ad0bc61b4775e28d029b03d496f57674e56e76ab
minhman <phminhman2501@gmail.com>
2023-10-23 21:25:45 +0700

    feat: add css to select and change file name

--- app/(routes)/(main)/(customer)/shop/product-list/ProductFilterSideBar.tsx
+++ app/(routes)/(main)/(customer)/shop/product-list/ProductFilterSideBar.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useCallback, type ChangeEvent } from 'react'
 import useQueryParams from '@/app/_hooks/useQueryParams'
+import { FunnelIcon } from '@heroicons/react/24/solid'
 
 type FieldQuery = {
 	size?: string
@@ -24,7 +25,7 @@ const options = {
 	type: typeOptions,
 }
 
-export default function FilterMenu() {
+function FilterMenu() {
 	const { queryParams, setQueryParams } = useQueryParams<QueryParams>()
 	const fieldQuery = queryParams?.get('field')
 	const object: FieldQuery = fieldQuery ? JSON.parse(fieldQuery) : {}
@@ -47,29 +48,48 @@ export default function FilterMenu() {
 		[setQueryParams, object],
 	)
 
-	const optionHandleChange = (event: ChangeEvent<HTMLSelectElement>) => {
+	const handleOptionChange = (event: ChangeEvent<HTMLSelectElement>) => {
 		filterSearch({ [event.target.name]: event.target.value })
 	}
 
 	return (
-		<>
+		<div className='flex-col-start gap-cozy'>
 			{Object.entries(options).map(([key, value]) => (
-				<select
+				<div
 					key={key}
-					onChange={optionHandleChange}
-					name={key}
+					className='grid grid-cols-6 gap-compact'
 				>
-					<option value=''>All</option>
-					{value.map((opt) => (
-						<option
-							key={opt}
-							value={opt}
-						>
-							{opt}
-						</option>
-					))}
-				</select>
+					<span className='col-span-2 flex items-center text-body-sm  capitalize text-primary-5555'>
+						{key}
+					</span>
+					<select
+						onChange={handleOptionChange}
+						name={key}
+						className='col-span-4 rounded-[4px] border-[1px] border-primary-625 bg-transparent px-cozy py-compact text-body-md text-primary-5555'
+					>
+						<option value=''>All</option>
+						{value.map((opt) => (
+							<option
+								key={opt}
+								value={opt}
+							>
+								{opt}
+							</option>
+						))}
+					</select>
+				</div>
 			))}
-		</>
+		</div>
+	)
+}
+
+export default function FilterSideBar() {
+	return (
+		<div className='max-h-screen rounded-[8px]  bg-white  p-comfortable shadow-63'>
+			<span className='mb-[16px] flex items-center gap-compact text-body-md text-primary-625'>
+				<h3>Filter</h3> <FunnelIcon className='aspect-square w-[24px]' />
+			</span>
+			<FilterMenu />
+		</div>
 	)
 }

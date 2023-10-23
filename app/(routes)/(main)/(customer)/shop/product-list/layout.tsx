396e5a28889620205ab00ef4419e6f774b9dd00f
minhman <phminhman2501@gmail.com>
2023-10-24 00:18:11 +0700

    feat: add metadata

--- app/(routes)/(main)/(customer)/shop/product-list/layout.tsx
+++ app/(routes)/(main)/(customer)/shop/product-list/layout.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import Banner from './Banner'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+	title: 'All Products',
+	description: 'All the products of GreenDeco',
+}
 
 export default function ProductListLayout({ children }: { children: React.ReactNode }) {
 	return (

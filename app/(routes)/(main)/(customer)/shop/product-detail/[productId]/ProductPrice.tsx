f46ba1e602a0ca0911691cf9563793750c5525a9
minhman <phminhman2501@gmail.com>
2023-10-25 18:13:35 +0700

    feat: init product price

--- app/(routes)/(main)/(customer)/shop/product-detail/[productId]/ProductPrice.tsx
+++ app/(routes)/(main)/(customer)/shop/product-detail/[productId]/ProductPrice.tsx
@@ -1,3 +1,13 @@
+import Button from '@/app/_components/Button'
+import { ShoppingCartIcon } from '@heroicons/react/24/solid'
 export default function Price() {
-	return <span>Price</span>
+	return (
+		<div className='flex items-center justify-between rounded-[8px] bg-primary-625 px-comfortable py-cozy'>
+			<span className='text-[3rem] font-semi-bold text-white'>$65</span>
+			<Button className='btnSecondary flex items-center gap-[4px]'>
+				<span>Add to cart</span>
+				<ShoppingCartIcon className='aspect-square w-[24px]' />
+			</Button>
+		</div>
+	)
 }

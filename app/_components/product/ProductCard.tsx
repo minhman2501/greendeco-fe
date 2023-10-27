ca598396eaa442428b0a802d895071df9975a786
minhman <phminhman2501@gmail.com>
2023-10-27 20:35:54 +0700

    feat: add handle click && hover css

--- app/_components/product/ProductCard.tsx
+++ app/_components/product/ProductCard.tsx
@@ -2,13 +2,18 @@ import Image from 'next/image'
 import { StarIcon } from '@heroicons/react/24/solid'
 import { ShoppingBagIcon } from '@heroicons/react/24/outline'
 import { ProductData } from '@/app/_api/axios/product'
+import { useRouter } from 'next/navigation'
 
 export type ProductCardProps = Pick<ProductData, 'id' | 'name' | 'images' | 'price'>
 
 export default function ProductCard({ product }: { product: ProductCardProps }) {
+	const productRouter = useRouter()
 	const { id, name, images, price } = product
 	return (
-		<span className='w-full overflow-hidden rounded-[8px] bg-white shadow-63'>
+		<span
+			className='w-full overflow-hidden rounded-[8px] border-transparent bg-white shadow-63 ease-linear hover:scale-110 hover:cursor-pointer '
+			onClick={() => productRouter.push(`/shop/product-detail/${id}`)}
+		>
 			<CardImage imageUrl={images[0]} />
 			<div className='flex w-full p-cozy'>
 				<CardDetail

9508808408a6cfb8fd39ec3ab5dad962f7bc6606
minhman <phminhman2501@gmail.com>
2023-10-27 23:30:55 +0700

    feat: implement getProductDetailById && pass props to ImageGallery

--- app/(routes)/(main)/(customer)/shop/product-detail/[productId]/page.tsx
+++ app/(routes)/(main)/(customer)/shop/product-detail/[productId]/page.tsx
@@ -1,7 +1,13 @@
+'use client'
+
+import { useQuery } from '@tanstack/react-query'
 import CommentSection from './ProductCommentSection'
 import DetailContainer from './ProductDetailContainer'
 import ImageGallery from './ProductImageGallery'
 import Price from './ProductPrice'
+import { VariantData } from '@/app/_api/axios/product'
+import { getProductDetailById } from '@/app/_api/axios/product'
+import { useState } from 'react'
 
 export default function ProductDetailPage({
 	params,
@@ -10,16 +16,31 @@ export default function ProductDetailPage({
 		productId: string
 	}
 }) {
+	const productDetailQuery = useQuery({
+		queryKey: ['product', params.productId],
+		queryFn: () => getProductDetailById(params.productId),
+		onSuccess: (data) => console.log(data),
+	})
+
+	const { data, isLoading, isSuccess, isError } = productDetailQuery
+
 	return (
-		<div className='flex-col-start gap-cozy'>
-			<ImageGallery />
-			<div className='grid grid-cols-2 gap-cozy'>
-				<DetailContainer />
+		<>
+			{data && (
 				<div className='flex-col-start gap-cozy'>
-					<Price />
-					<CommentSection />
+					<ImageGallery
+						variantImage={data.variants[0].image}
+						productImages={data.product.images}
+					/>
+					<div className='grid grid-cols-2 gap-cozy'>
+						<DetailContainer />
+						<div className='flex-col-start gap-cozy'>
+							<Price />
+							<CommentSection />
+						</div>
+					</div>
 				</div>
-			</div>
-		</div>
+			)}
+		</>
 	)
 }

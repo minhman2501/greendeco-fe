5e4dc11ddedfd95aead31eae8514f0122335792f
minhman <phminhman2501@gmail.com>
2023-10-29 12:07:30 +0700

    feat: change css && enable sticky

--- app/(routes)/(main)/(customer)/shop/product-detail/[productId]/ProductDetailContainer.tsx
+++ app/(routes)/(main)/(customer)/shop/product-detail/[productId]/ProductDetailContainer.tsx
@@ -22,7 +22,7 @@ export type DetailContainerProps = {
 
 export default function DetailContainer({ product, variantList }: DetailContainerProps) {
 	return (
-		<div className='flex-col-start gap-cozy rounded-[8px] border-[1px] border-primary-580 bg-primary-580-20 p-comfortable'>
+		<div className='flex-col-start sticky top-comfortable h-fit gap-cozy rounded-[8px] border-[1px] border-primary-580 bg-primary-580-20 p-comfortable'>
 			<div className='flex items-center justify-between gap-cozy border-b-[1px] border-primary-5555-80 pb-cozy'>
 				<h1 className='text-heading text-primary-625'>{product.name}</h1>
 				<HeartIcon className='aspect-square w-[36px]' />

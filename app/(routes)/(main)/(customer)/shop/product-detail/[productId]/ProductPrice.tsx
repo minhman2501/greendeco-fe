c983b2715dad8a0002844ef2be4ae19ce37196fa
minhman <phminhman2501@gmail.com>
2023-10-29 17:55:49 +0700

    feat: change css

--- app/(routes)/(main)/(customer)/shop/product-detail/[productId]/ProductPrice.tsx
+++ app/(routes)/(main)/(customer)/shop/product-detail/[productId]/ProductPrice.tsx
@@ -9,7 +9,7 @@ export default function Price() {
 		<div className='flex items-center justify-between rounded-[8px] bg-primary-625 px-comfortable py-cozy'>
 			<span className='text-[3rem] font-semi-bold text-white'>{`${price} ${currency}`}</span>
 			<Button className='btnSecondary flex items-center gap-[4px]'>
-				<span>Add to cart</span>
+				<span className='font-semi-bold'>Add to cart</span>
 				<ShoppingCartIcon className='aspect-square w-[24px]' />
 			</Button>
 		</div>

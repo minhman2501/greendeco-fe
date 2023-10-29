845c66b8203f739bc6cc5c8a3c0077c08aa4be94
minhman <phminhman2501@gmail.com>
2023-10-29 16:50:29 +0700

    feat: change props and add star

--- app/_configs/schemas/review.ts
+++ app/_configs/schemas/review.ts
@@ -2,11 +2,12 @@ import { MIN_PASSWORD, MAX_PASSWORD } from '../constants/variables'
 import * as z from 'zod'
 
 export const ReviewSchema = z.object({
-	email: z
+	review: z
 		.string()
 		.min(20, 'Your comment needs to have more than 20 characters')
 		.max(100, 'No more than 100 characters')
 		.email('Email is invalid'),
+	star: z.number().min(0).max(5, 'You can only rate up to 5 star only'),
 })
 
 export type ReviewInputType = z.infer<typeof ReviewSchema>

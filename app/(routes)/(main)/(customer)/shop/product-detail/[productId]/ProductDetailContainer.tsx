4c2f39d19229718cc31674b95d2b50e645555224
minhman <phminhman2501@gmail.com>
2023-10-30 16:49:30 +0700

    refactor: change props and use destructuring

--- app/(routes)/(main)/(customer)/shop/product-detail/[productId]/ProductDetailContainer.tsx
+++ app/(routes)/(main)/(customer)/shop/product-detail/[productId]/ProductDetailContainer.tsx
@@ -1,30 +1,17 @@
-import { HeartIcon } from '@heroicons/react/24/outline'
-import Link from 'next/link'
 import { VariantList } from './components'
 import { ProductData, VariantData } from '@/app/_api/axios/product'
 
 export type DetailContainerProps = {
-	product: Pick<
-		ProductData,
-		| 'id'
-		| 'detail'
-		| 'description'
-		| 'type'
-		| 'size'
-		| 'name'
-		| 'difficulty'
-		| 'light'
-		| 'water'
-		| 'default_variant'
-	>
+	product: ProductData
 	variantList: VariantData[]
 }
 
 export default function DetailContainer({ product, variantList }: DetailContainerProps) {
+	const { name, description, size, difficulty, light, water } = product
 	return (
 		<div className='flex-col-start sticky top-comfortable h-fit gap-cozy rounded-[8px]  bg-primary-580-20 p-comfortable shadow-18'>
 			<div className='flex items-center justify-between gap-cozy border-b-[1px] border-primary-5555-80 pb-cozy'>
-				<h1 className='text-heading text-primary-625'>{product.name}</h1>
+				<h1 className='text-heading text-primary-625'>{name}</h1>
 				{/* <HeartIcon className='aspect-square w-[36px]' /> */}
 			</div>
 			<div className='flex-col-start gap-compact'>
@@ -45,29 +32,29 @@ export default function DetailContainer({ product, variantList }: DetailContaine
 						Read More
 					</Link> */}
 				</span>
-				<p className='w-full text-justify text-body-sm'>{product.description}</p>
+				<p className='w-full text-justify text-body-sm'>{description}</p>
 			</div>
 			<div className='flex-col-start gap-compact'>
 				<h2 className='text-body-lg font-semi-bold capitalize text-primary-418'>Size</h2>
-				<p className='w-full text-justify text-body-sm'>{product.size}</p>
+				<p className='w-full text-justify text-body-sm'>{size}</p>
 			</div>
 			<div className='flex-col-start gap-compact'>
 				<h2 className='text-body-lg font-semi-bold capitalize text-primary-418'>
 					Caring difficulty
 				</h2>
-				<p className='w-full text-justify text-body-sm capitalize'>{product.difficulty}</p>
+				<p className='w-full text-justify text-body-sm capitalize'>{difficulty}</p>
 			</div>
 			<div className='flex-col-start gap-compact'>
 				<h2 className='text-body-lg font-semi-bold capitalize text-primary-418'>
 					Ideal Light Condition
 				</h2>
-				<p className='w-full text-justify text-body-sm capitalize'>{product.light}</p>
+				<p className='w-full text-justify text-body-sm capitalize'>{light}</p>
 			</div>
 			<div className='flex-col-start gap-compact'>
 				<h2 className='text-body-lg font-semi-bold capitalize text-primary-418'>
 					Watering Rountine
 				</h2>
-				<p className='w-full text-justify text-body-sm capitalize'>{product.water}</p>
+				<p className='w-full text-justify text-body-sm capitalize'>{water}</p>
 			</div>
 		</div>
 	)

4b14d369ba1b31cd8318cd8cc2f0359e22d4d11d
minhman <phminhman2501@gmail.com>
2023-10-30 16:31:41 +0700

    feat: handle generate metadata && migrate layout

--- app/(routes)/(main)/(customer)/shop/product-detail/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ProductDetailLayout({ children }: { children: React.ReactNode }) {
-	return (
-		<div className='w-screen bg-light-green pt-[80px]'>
-			<div className='container'>{children}</div>
-		</div>
-	)
-}

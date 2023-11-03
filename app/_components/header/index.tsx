7273aa905584c9b4f4ea8b6fd6e59ac2dc3d3ece
minhman <phminhman2501@gmail.com>
2023-11-03 19:58:50 +0700

    feat: develope components

--- app/_components/header/index.tsx
+++ app/_components/header/index.tsx
@@ -6,58 +6,49 @@ export default function Header() {
 	return (
 		<header className='sticky top-cozy z-20'>
 			<div className='container'>
-				<div className='mt-[-60px] flex h-[60px] items-center gap-cozy'>
-					<span className='aspect-square h-full  bg-primary-625'></span>
-					<div className='flex-col-start h-full flex-1 justify-center border-[1px] border-primary-625 bg-white px-cozy shadow-15'>
-						<div className='flex items-center justify-between'>
-							<ul className='flex list-none text-body-sm text-primary-625'>
-								<li>
-									<Link
-										href={'/'}
-										className='px-cozy text-primary-625'
-									>
-										Plants
-									</Link>
-								</li>
-								<li>
-									<Link
-										href={'/'}
-										className='px-cozy text-primary-625'
-									>
-										Plants
-									</Link>
-								</li>
-								<li>
-									<Link
-										href={'/'}
-										className='px-cozy text-primary-625'
-									>
-										Plants
-									</Link>
-								</li>
-							</ul>
-							<ul className='flex list-none gap-cozy'>
-								<li>
-									<span className='border-[1px] border-gray-500 px-compact py-[4px]'>
-										lmao
-									</span>
-								</li>
-								<li>
-									<span className='border-[1px] border-gray-500 px-compact py-[4px]'>
-										lmao
-									</span>
-								</li>
-								<li>
-									<span className='border-[1px] border-gray-500 px-compact py-[4px]'>
-										lmao
-									</span>
-								</li>
-							</ul>
-						</div>
-					</div>
+				<div className='mx-[-20px] mt-[-60px] flex h-[60px] items-center gap-cozy'>
+					<Logo />
+					<NavBar />
 					<Button className='h-full px-cozy'>Login</Button>
 				</div>
 			</div>
 		</header>
+	)
+}
+
+function Logo() {
+	return <span className='aspect-square h-full  bg-primary-625'></span>
+}
+
+function NavBar() {
+	return (
+		<div className='flex-col-start h-full flex-1 justify-center border-[1px] border-primary-625 bg-white px-cozy shadow-15'>
+			<div className='flex items-center justify-between'>
+				<NavigationList />
+			</div>
+		</div>
+	)
+}
+
+function NavigationList() {
+	return (
+		<ul className='flex list-none text-body-sm text-primary-625'>
+			<li>
+				<Link
+					href={'/shop'}
+					className='px-cozy text-primary-625'
+				>
+					Plants
+				</Link>
+			</li>
+			<li>
+				<Link
+					href={'/'}
+					className='px-cozy text-primary-625'
+				>
+					About Us
+				</Link>
+			</li>
+		</ul>
 	)
 }

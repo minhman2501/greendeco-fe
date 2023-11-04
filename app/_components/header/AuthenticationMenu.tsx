ed88753186f68904832a8613fcf10fed3a34815a
minhman <phminhman2501@gmail.com>
2023-11-04 17:41:26 +0700

    feat: enable go to user setting page

--- app/_components/header/AuthenticationMenu.tsx
+++ app/_components/header/AuthenticationMenu.tsx
@@ -4,12 +4,14 @@ import Image from 'next/image'
 import { UserProfileResponseData, getUserProfile } from '@/app/_api/axios/user'
 import { ACCESS_TOKEN_COOKIE_NAME } from '@/app/_configs/constants/cookies'
 import { UseQueryKeys } from '@/app/_configs/constants/queryKey'
-import { ChevronDownIcon } from '@heroicons/react/24/solid'
+import { ChevronDownIcon, Cog8ToothIcon } from '@heroicons/react/24/solid'
 import { useQuery } from '@tanstack/react-query'
 import { getCookie } from 'cookies-next'
 import Button from '../Button'
 import { useRouter } from 'next/navigation'
 import { DEFAULT_AVATAR } from '@/app/_configs/constants/images'
+import React, { useRef, useState } from 'react'
+import clsx from 'clsx'
 
 export default function AuthenticationHandler() {
 	const accessToken = getCookie(ACCESS_TOKEN_COOKIE_NAME)
@@ -24,7 +26,7 @@ export default function AuthenticationHandler() {
 	const { data, isLoading, isSuccess, isError } = userQuery
 
 	return (
-		<div className='h-full w-[220px] overflow-hidden rounded-[8px] bg-primary-625 shadow-30'>
+		<div className='h-full w-[220px] rounded-[8px] bg-primary-625 shadow-30'>
 			{isSuccess && <UserSettingMenu {...data} />}
 			{isError && (
 				<Button
@@ -38,7 +40,34 @@ export default function AuthenticationHandler() {
 	)
 }
 
-function UserSettingMenu({ avatar, firstName, lastName }: UserProfileResponseData) {
+function UserSettingMenu(props: UserProfileResponseData) {
+	const router = useRouter()
+	const [isOpen, setIsOpen] = useState(false)
+	return (
+		<div
+			onClick={() => setIsOpen(!isOpen)}
+			className='relative h-full w-full'
+		>
+			<MenuButton {...props} />
+
+			{isOpen && (
+				<ul
+					className='absolute inset-x-0 top-[calc(100%+8px)] z-30 rounded-[8px] border-[1px] border-primary-5555-40 bg-white p-compact 
+                    shadow-38'
+				>
+					<MenuItem onClick={() => router.push('/user/setting/profile')}>
+						<div className='flex h-full w-full items-center gap-compact'>
+							<Cog8ToothIcon className='aspect-square h-[16px]' />
+							user setting
+						</div>
+					</MenuItem>
+				</ul>
+			)}
+		</div>
+	)
+}
+
+function MenuButton({ avatar, firstName, lastName }: UserProfileResponseData) {
 	return (
 		<span className='flex h-full max-w-full items-center justify-center gap-compact px-cozy py-[12px] '>
 			<span className='aspect-square h-full overflow-hidden rounded-[100%]'>
@@ -55,5 +84,27 @@ function UserSettingMenu({ avatar, firstName, lastName }: UserProfileResponseDat
 			</span>
 			<ChevronDownIcon className='aspect-square h-[24px] text-white' />
 		</span>
+	)
+}
+
+function MenuItem({
+	onClick,
+	children,
+	className,
+}: {
+	onClick?: () => void
+	children: React.ReactNode
+	className?: string
+}) {
+	return (
+		<li
+			className={clsx(
+				'w-full cursor-pointer rounded-[8px] px-[8px] py-[12px] text-body-sm capitalize hover:bg-primary-5555-20/40',
+				className,
+			)}
+			onClick={onClick}
+		>
+			{children}
+		</li>
 	)
 }

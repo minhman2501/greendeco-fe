33b96fca1dd3629beb8c128ed097e661637990c9
minhman <phminhman2501@gmail.com>
2023-11-06 15:27:59 +0700

    feat: add response data type

--- app/_api/axios/cart.ts
+++ app/_api/axios/cart.ts
@@ -1,6 +1,46 @@
+import { AccessTokenType } from '@/app/_types'
 import axios from 'axios'
+import { UserProfileResponseData } from './user'
+import { VariantData } from './product'
 
 const CART_URL = `${process.env.NEXT_PUBLIC_GREENDECO_BACKEND_API}/cart`
+
+type CartInfoData = {
+	id: string
+	owner: UserProfileResponseData['id']
+	description: string
+	created_at: string
+	updated_at: string
+}
+
+type CartInfoFromUserResponseData = {
+	items: CartInfoData
+	page: number
+	page_size: number
+	next: Boolean
+	prev: Boolean
+}
+
+type CreateNewCartResponseData = {
+	id: CartInfoData['id']
+}
+
+type CartItemData = {
+	id: string
+	cart: CartInfoData['id']
+	variant: VariantData['id']
+	quantity: number
+	created_at: string
+	updated_at: string
+}
+
+type CartItemListResponseData = {
+	items: CartItemData[]
+	page: number
+	page_size: number
+	next: Boolean
+	prev: Boolean
+}
 
 export const cartApi = axios.create({
 	baseURL: CART_URL,
@@ -8,32 +48,32 @@ export const cartApi = axios.create({
 
 cartApi.defaults.headers.common['Content-Type'] = 'application/json'
 
-export const getCartFromUser = async (accessToken: string) => {
+export const getCartInfoFromUser = async (accessToken: AccessTokenType) => {
 	return await cartApi
-		.get('', {
+		.get<CartInfoFromUserResponseData>('', {
 			headers: {
 				Authorization: `Bearer ${accessToken}`,
 			},
 		})
-		.then((res) => res)
+		.then((res) => res.data)
 }
 
-export const createNewCart = async (accessToken: string) => {
+export const createNewCart = async (accessToken: AccessTokenType) => {
 	return await cartApi
-		.post('', {
+		.post<CreateNewCartResponseData>('', {
 			headers: {
 				Authorization: `Bearer ${accessToken}`,
 			},
 		})
-		.then((res) => res)
+		.then((res) => res.data)
 }
 
-export const getCartItemsFromCartId = async (cartId: string, accessToken: string) => {
+export const getCartItemListFromCartId = async (cartId: string, accessToken: AccessTokenType) => {
 	return await cartApi
-		.get(`/${cartId}`, {
+		.get<CartItemListResponseData>(`/${cartId}`, {
 			headers: {
 				Authorization: `Bearer ${accessToken}`,
 			},
 		})
-		.then((res) => res)
+		.then((res) => res.data)
 }
---
+++ app/_types/index.ts
@@ -0,0 +1,1 @@
+export type AccessTokenType = string | undefined

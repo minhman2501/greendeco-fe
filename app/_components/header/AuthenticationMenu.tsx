f17450622d2ebcb7450382556d5fd2b611ed9106
minhman <phminhman2501@gmail.com>
2023-11-06 15:05:57 +0700

    feat: enable log out

--- app/_components/header/AuthenticationMenu.tsx
+++ app/_components/header/AuthenticationMenu.tsx
@@ -4,13 +4,13 @@ import Image from 'next/image'
 import { UserProfileResponseData, getUserProfile } from '@/app/_api/axios/user'
 import { ACCESS_TOKEN_COOKIE_NAME } from '@/app/_configs/constants/cookies'
 import { UseQueryKeys } from '@/app/_configs/constants/queryKey'
-import { ChevronDownIcon, Cog8ToothIcon } from '@heroicons/react/24/solid'
+import { ArrowLeftOnRectangleIcon, ChevronDownIcon, Cog8ToothIcon } from '@heroicons/react/24/solid'
 import { useQuery } from '@tanstack/react-query'
-import { getCookie } from 'cookies-next'
+import { deleteCookie, getCookie } from 'cookies-next'
 import Button from '../Button'
 import { useRouter } from 'next/navigation'
 import { DEFAULT_AVATAR } from '@/app/_configs/constants/images'
-import React, { useRef, useState } from 'react'
+import React, { useState } from 'react'
 import clsx from 'clsx'
 
 export default function AuthenticationHandler() {
@@ -30,7 +30,7 @@ export default function AuthenticationHandler() {
 			{isSuccess && <UserSettingMenu {...data} />}
 			{isError && (
 				<Button
-					className='h-full px-[20px]'
+					className='h-full w-full'
 					onClick={() => router.push('/login')}
 				>
 					Login
@@ -43,6 +43,11 @@ export default function AuthenticationHandler() {
 function UserSettingMenu(props: UserProfileResponseData) {
 	const router = useRouter()
 	const [isOpen, setIsOpen] = useState(false)
+
+	const handleLogOut = () => {
+		deleteCookie(ACCESS_TOKEN_COOKIE_NAME)
+		router.push('login')
+	}
 	return (
 		<div
 			onClick={() => setIsOpen(!isOpen)}
@@ -51,14 +56,17 @@ function UserSettingMenu(props: UserProfileResponseData) {
 			<MenuButton {...props} />
 
 			{isOpen && (
-				<ul
-					className='absolute inset-x-0 top-[calc(100%+8px)] z-30 rounded-[8px] border-[1px] border-primary-5555-40 bg-white p-compact 
-                    shadow-38'
-				>
+				<ul className='absolute inset-x-0 top-[calc(100%+8px)] z-30 rounded-[8px] border-[1px] border-primary-5555-40 bg-white p-compact'>
 					<MenuItem onClick={() => router.push('/user/setting/profile')}>
 						<div className='flex h-full w-full items-center gap-compact'>
 							<Cog8ToothIcon className='aspect-square h-[16px]' />
 							user setting
+						</div>
+					</MenuItem>
+					<MenuItem onClick={() => handleLogOut()}>
+						<div className='flex h-full w-full items-center gap-compact'>
+							<ArrowLeftOnRectangleIcon className='aspect-square h-[16px]' />
+							log out
 						</div>
 					</MenuItem>
 				</ul>
@@ -69,7 +77,7 @@ function UserSettingMenu(props: UserProfileResponseData) {
 
 function MenuButton({ avatar, firstName, lastName }: UserProfileResponseData) {
 	return (
-		<span className='flex h-full max-w-full items-center justify-center gap-compact px-cozy py-[12px] '>
+		<span className='flex h-full max-w-full cursor-pointer items-center justify-center gap-compact  px-cozy py-[12px] '>
 			<span className='aspect-square h-full overflow-hidden rounded-[100%]'>
 				<Image
 					width={0}

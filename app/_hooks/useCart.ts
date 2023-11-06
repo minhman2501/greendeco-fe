9d846e48cd80eb34247984e12b30c3de0c8cff53
minhman <phminhman2501@gmail.com>
2023-11-06 17:59:27 +0700

    feat: create useCart hook, develop handle get cartid and get card list item

---
+++ app/_hooks/useCart.ts
@@ -0,0 +1,29 @@
+import {
+	getCartInfoFromUser,
+	getCartItemListFromCartId,
+	CartInfoFromUserResponseData,
+	createNewCart,
+	cartApi,
+} from '../_api/axios/cart'
+import { getCookie } from 'cookies-next'
+import { ACCESS_TOKEN_COOKIE_NAME } from '../_configs/constants/cookies'
+import { AxiosError } from 'axios'
+
+const userAccessToken = getCookie(ACCESS_TOKEN_COOKIE_NAME)?.toString()
+
+export const handleGetCartId = async () => {
+	return await getCartInfoFromUser(userAccessToken)
+		.then((data) => data.items.id)
+		.catch((e: AxiosError) => {
+			if (e.response?.status === 404) {
+				console.log('not found')
+				return createNewCart(userAccessToken).then((data) => data.id)
+			}
+		})
+		.then((data) => data)
+}
+
+export const handleGetCartItemListByCartId = async () => {
+	const cartId = await handleGetCartId().then((data) => data)
+	if (cartId) return getCartItemListFromCartId(cartId, userAccessToken).then((data) => data)
+}

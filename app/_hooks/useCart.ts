c2ea580ff5d59221bde3123bbed63f691de5fc70
minhman <phminhman2501@gmail.com>
2023-11-07 13:19:58 +0700

    feat: implement the query and mutation

--- app/_hooks/useCart.ts
+++ app/_hooks/useCart.ts
@@ -1,23 +1,85 @@
-import { getCartInfoFromUser, getCartItemListFromCartId, createNewCart } from '../_api/axios/cart'
+'use client'
+
+import {
+	getCartInfoFromUser,
+	getCartItemListFromCartId,
+	createNewCart,
+	changeCartItemQuantity,
+	removeCartItem,
+	clearCartItemList,
+	addCartItem,
+} from '../_api/axios/cart'
 import { getCookie } from 'cookies-next'
+import { useQueryClient, useMutation, useQuery } from '@tanstack/react-query'
 import { ACCESS_TOKEN_COOKIE_NAME } from '../_configs/constants/cookies'
 import { AxiosError } from 'axios'
+import { setCookie } from 'cookies-next'
+import { AccessTokenType } from '../_types'
 
-const userAccessToken = getCookie(ACCESS_TOKEN_COOKIE_NAME)?.toString()
-
-export const handleGetCartId = async () => {
-	return await getCartInfoFromUser(userAccessToken)
+export const handleGetCartId = async (accessToken: AccessTokenType) => {
+	return await getCartInfoFromUser(accessToken)
 		.then((data) => data.items.id)
 		.catch((e: AxiosError) => {
 			if (e.response?.status === 404) {
-				console.log('not found')
-				return createNewCart(userAccessToken).then((data) => data.id)
+				return createNewCart(accessToken).then((data) => data.id)
 			}
 		})
 		.then((data) => data)
 }
 
 export const handleGetCartItemListByCartId = async () => {
-	const cartId = await handleGetCartId().then((data) => data)
-	if (cartId) return getCartItemListFromCartId(cartId, userAccessToken).then((data) => data)
+	const accessToken = getCookie(ACCESS_TOKEN_COOKIE_NAME)
+	const cartId = await handleGetCartId(accessToken).then((data) => data)
+	if (cartId) {
+		//NOTE: Save the cartId for further features like change item quantity, add new items...
+		setCookie('cartId', cartId)
+		return getCartItemListFromCartId(cartId, accessToken).then((data) => data)
+	}
+}
+
+export default function useCart() {
+	const queryClient = useQueryClient()
+
+	const cartQuery = useQuery({
+		queryKey: ['cart'],
+		queryFn: handleGetCartItemListByCartId,
+		onError: (e) => console.log(e),
+		retry: false,
+	})
+
+	const changeQuantityMutation = useMutation({
+		mutationFn: changeCartItemQuantity,
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ['cart'] })
+		},
+	})
+
+	const removeCartItemMutation = useMutation({
+		mutationFn: removeCartItem,
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ['cart'] })
+		},
+	})
+
+	const addCartItemMutation = useMutation({
+		mutationFn: addCartItem,
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ['cart'] })
+		},
+	})
+
+	const clearCartItemMutation = useMutation({
+		mutationFn: clearCartItemList,
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ['cart'] })
+		},
+	})
+
+	return {
+		cartQuery: cartQuery,
+		addCartItemMutation: addCartItemMutation,
+		changeQuantityMutation: changeQuantityMutation,
+		removeCartItemMutation: removeCartItemMutation,
+		clearCartItemMutation: clearCartItemMutation,
+	}
 }

33366733005c9dcfb26f379f96614b24225343c8
minhman <phminhman2501@gmail.com>
2023-11-07 13:53:21 +0700

    feat: develop add item to cart

--- app/_hooks/useCart.ts
+++ app/_hooks/useCart.ts
@@ -6,38 +6,42 @@ import {
 	createNewCart,
 	changeCartItemQuantity,
 	removeCartItem,
+	CartInfoData,
 	clearCartItemList,
 	addCartItem,
 } from '../_api/axios/cart'
+import { VariantData } from '../_api/axios/product'
 import { getCookie } from 'cookies-next'
 import { useQueryClient, useMutation, useQuery } from '@tanstack/react-query'
 import { ACCESS_TOKEN_COOKIE_NAME } from '../_configs/constants/cookies'
 import { AxiosError } from 'axios'
 import { setCookie } from 'cookies-next'
 import { AccessTokenType } from '../_types'
-
-export const handleGetCartId = async (accessToken: AccessTokenType) => {
-	return await getCartInfoFromUser(accessToken)
-		.then((data) => data.items.id)
-		.catch((e: AxiosError) => {
-			if (e.response?.status === 404) {
-				return createNewCart(accessToken).then((data) => data.id)
-			}
-		})
-		.then((data) => data)
-}
-
-export const handleGetCartItemListByCartId = async () => {
-	const accessToken = getCookie(ACCESS_TOKEN_COOKIE_NAME)
-	const cartId = await handleGetCartId(accessToken).then((data) => data)
-	if (cartId) {
-		//NOTE: Save the cartId for further features like change item quantity, add new items...
-		setCookie('cartId', cartId)
-		return getCartItemListFromCartId(cartId, accessToken).then((data) => data)
-	}
-}
+import { CartItemData } from '../_api/axios/cart'
 
 export default function useCart() {
+	//NOTE: Handle getCartId - if there isn't any cart -> create new one
+	const handleGetCartId = async (accessToken: AccessTokenType) => {
+		return await getCartInfoFromUser(accessToken)
+			.then((data) => data.items.id)
+			.catch((e: AxiosError) => {
+				if (e.response?.status === 404) {
+					return createNewCart(accessToken).then((data) => data.id)
+				}
+			})
+			.then((data) => data)
+	}
+
+	const handleGetCartItemListByCartId = async () => {
+		const accessToken = getCookie(ACCESS_TOKEN_COOKIE_NAME)
+		const cartId = await handleGetCartId(accessToken).then((data) => data)
+		if (cartId) {
+			//NOTE: Save the cartId for further features like change item quantity, add new items...
+			setCookie('cartId', cartId)
+			return getCartItemListFromCartId(cartId, accessToken).then((data) => data)
+		}
+	}
+
 	const queryClient = useQueryClient()
 
 	const cartQuery = useQuery({
@@ -45,6 +49,13 @@ export default function useCart() {
 		queryFn: handleGetCartItemListByCartId,
 		onError: (e) => console.log(e),
 		retry: false,
+	})
+
+	const addCartItemMutation = useMutation({
+		mutationFn: addCartItem,
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ['cart'] })
+		},
 	})
 
 	const changeQuantityMutation = useMutation({
@@ -61,13 +72,6 @@ export default function useCart() {
 		},
 	})
 
-	const addCartItemMutation = useMutation({
-		mutationFn: addCartItem,
-		onSuccess: () => {
-			queryClient.invalidateQueries({ queryKey: ['cart'] })
-		},
-	})
-
 	const clearCartItemMutation = useMutation({
 		mutationFn: clearCartItemList,
 		onSuccess: () => {
@@ -75,11 +79,71 @@ export default function useCart() {
 		},
 	})
 
+	const handleAddCartItem = (
+		cart_id: CartInfoData['id'],
+		quantity: CartItemData['quantity'],
+		variant_id: VariantData['id'],
+	) => {
+		const accessToken = getCookie(ACCESS_TOKEN_COOKIE_NAME)?.toString()
+
+		addCartItemMutation.mutate({
+			itemData: {
+				variant_id: variant_id,
+				cart_id: cart_id,
+				quantity: quantity,
+			},
+			accessToken: accessToken,
+		})
+	}
+
+	const handleIncreaseQuantity = (
+		itemId: CartItemData['id'],
+		quantity: CartItemData['quantity'],
+	) => {
+		const accessToken = getCookie(ACCESS_TOKEN_COOKIE_NAME)?.toString()
+
+		changeQuantityMutation.mutate({
+			itemId: itemId,
+			quantity: quantity + 1,
+			accessToken: accessToken,
+		})
+	}
+
+	const handleDecreaseQuantity = (
+		itemId: CartItemData['id'],
+		quantity: CartItemData['quantity'],
+	) => {
+		const accessToken = getCookie(ACCESS_TOKEN_COOKIE_NAME)?.toString()
+		if (quantity > 1)
+			changeQuantityMutation.mutate({
+				itemId: itemId,
+				quantity: quantity - 1,
+				accessToken: accessToken,
+			})
+	}
+
+	const handleRemoveCartItem = (itemId: CartItemData['id']) => {
+		const accessToken = getCookie(ACCESS_TOKEN_COOKIE_NAME)
+		removeCartItemMutation.mutate({
+			itemId: itemId,
+			accessToken: accessToken,
+		})
+	}
+
+	const handleClearCartList = (cartId: string) => {
+		const accessToken = getCookie(ACCESS_TOKEN_COOKIE_NAME)
+		clearCartItemMutation.mutate({
+			cartId: cartId,
+			accessToken: accessToken,
+		})
+	}
+
 	return {
-		cartQuery: cartQuery,
-		addCartItemMutation: addCartItemMutation,
-		changeQuantityMutation: changeQuantityMutation,
-		removeCartItemMutation: removeCartItemMutation,
-		clearCartItemMutation: clearCartItemMutation,
+		cartQuery: { ...cartQuery },
+		addCartItem: handleAddCartItem,
+		increaseQuantity: handleIncreaseQuantity,
+		decreaseQuantity: handleDecreaseQuantity,
+		removeCartItem: handleRemoveCartItem,
+		clearCartItemMutation: handleClearCartList,
 	}
 }

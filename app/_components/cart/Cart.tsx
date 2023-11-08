473b73aeb8e33f4eb82f1d945beff272c17062b6
minhman <phminhman2501@gmail.com>
2023-11-08 11:39:19 +0700

    feat: implement new layotu

--- app/_components/cart/Cart.tsx
+++ app/_components/cart/Cart.tsx
@@ -3,6 +3,7 @@ import useCart from '@/app/_hooks/useCart'
 import clsx from 'clsx'
 import CartItem from './CartItem'
 import { getVariantById } from '@/app/_api/axios/product'
+import CartList from './CartList'
 
 export default function Cart() {
 	const { cartQuery } = useCart()
@@ -10,24 +11,12 @@ export default function Cart() {
 	const { isSuccess, data, isLoading } = cartQuery
 
 	return (
-		<div
-			className={clsx({
-				'opacity-70': isLoading,
-			})}
-		>
-			<div>Cart</div>
-			{data && (
-				<div>
-					{data.items.map((item, index) => (
-						<>
-							<CartItem
-								key={item.id}
-								cartItem={{ ...item }}
-							/>
-						</>
-					))}
-				</div>
-			)}
+		<div className='h-full max-h-full w-[550px] overflow-hidden rounded-[8px]  bg-white p-comfortable shadow-26'>
+			<div>
+				<h3 className='mb-[4px] text-heading-1 capitalize text-primary-5555'>Your Cart</h3>
+				<p className='text-body-md text-primary-5555-80'>All of your finest choices.</p>
+			</div>
+			{data && <CartList {...data} />}
 		</div>
 	)
 }

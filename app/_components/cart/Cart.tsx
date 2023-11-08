8b8b76c9edf188eba99763dea3f55be915e87656
minhman <phminhman2501@gmail.com>
2023-11-08 14:58:41 +0700

    feat: implement CartCalculator

--- app/_components/cart/Cart.tsx
+++ app/_components/cart/Cart.tsx
@@ -1,10 +1,8 @@
 'use client'
 import useCart from '@/app/_hooks/useCart'
 import clsx from 'clsx'
-import CartItem from './CartItem'
-import { getVariantById } from '@/app/_api/axios/product'
 import CartList from './CartList'
-import CartCalculator from './SubTotalCalculator'
+import CartCalculator from './CartCalculator'
 
 export default function Cart() {
 	const { cartQuery } = useCart()
@@ -17,16 +15,22 @@ export default function Cart() {
 				<h3 className='mb-[4px] text-heading-1 capitalize text-primary-5555'>Your Cart</h3>
 				<p className='text-body-md text-primary-5555-80'>All of your finest choices.</p>
 			</div>
-			<div
-				className={clsx('flex-1 overflow-y-auto', {
-					'opacity-60': isLoading,
-				})}
-			>
-				{data && <CartList {...data} />}
-			</div>
-			<div className='p-comfortable pt-cozy'>
-				<CartCalculator />
-			</div>
+			{data && data.page_size > 0 && (
+				<>
+					<div
+						className={clsx('flex-1 overflow-y-auto', {
+							'opacity-60': isLoading,
+						})}
+					>
+						<CartList {...data} />
+					</div>
+					<div className='p-comfortable pt-cozy'>
+						<CartCalculator {...data} />
+					</div>
+				</>
+			)}
 		</div>
 	)
 }
+
+function NoItemMessage() {}

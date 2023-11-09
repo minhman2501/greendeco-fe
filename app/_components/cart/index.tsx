b1cd1e07fcf08b81d9dc0e071ef6123120cba22f
minhman <phminhman2501@gmail.com>
2023-11-09 14:01:02 +0700

    feat: init Cart Dialog

---
+++ app/_components/cart/index.tsx
@@ -0,0 +1,9 @@
+import Cart from './Cart'
+
+export default function CartDialog() {
+	return (
+		<div className='container sticky inset-0 flex justify-end p-comfortable'>
+			<Cart />
+		</div>
+	)
+}

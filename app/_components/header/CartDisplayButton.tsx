79af82a7b30a7f452c702573f303d4ef7fb9932d
minhman <phminhman2501@gmail.com>
2023-11-15 20:23:19 +0700

    feat: enable display cart list item amount

--- app/_components/header/CartDisplayButton.tsx
+++ app/_components/header/CartDisplayButton.tsx
@@ -11,7 +11,7 @@ export default function CartDisplayButton() {
 	const { cartQuery } = useCartQuery()
 	const { openCart } = useCartDialog()
 
-	const { isSuccess, isError } = cartQuery
+	const { isSuccess, isError, data } = cartQuery
 
 	const handleCartButtonOnClick = () => {
 		if (isSuccess) openCart()
@@ -21,9 +21,19 @@ export default function CartDisplayButton() {
 	return (
 		<button
 			onClick={handleCartButtonOnClick}
-			className='rounded-xl border-[1px] border-primary-5555-40 bg-primary-5555-20/40 px-[8px] py-[4px] text-primary-625 hover:bg-primary-625 hover:text-neutral-gray-1'
+			className='group relative rounded-xl border-[1px] border-primary-5555-40 bg-primary-5555-20/40 px-[8px] py-[4px] text-primary-625 transition duration-75 ease-in hover:bg-primary-625 hover:text-neutral-gray-1'
 		>
+			{isSuccess && CartAmountDisplay(data?.page_size)}
 			<ShoppingCartIcon className='aspect-square h-[24px] ' />
 		</button>
 	)
+}
+
+function CartAmountDisplay(amount: number | undefined) {
+	if (amount && amount > 0)
+		return (
+			<span className='absolute right-[-4px] top-[-4px] flex aspect-square h-[16px] items-center justify-center rounded-[100%] border-[1px] border-primary-625 bg-primary-625 font-semi-bold text-white transition duration-75  ease-in group-hover:bg-white group-hover:text-primary-625'>
+				{amount}
+			</span>
+		)
 }

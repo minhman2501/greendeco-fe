501151ba6c3aaadf8ad618b73fd646205787ce88
minhman <phminhman2501@gmail.com>
2023-11-16 10:44:05 +0700

    feat: init header with logo full

--- app/(routes)/(main)/administrator/(main)/layout.tsx
+++ app/(routes)/(main)/administrator/(main)/layout.tsx
@@ -1,9 +1,10 @@
 import { ReactNode } from 'react'
+import { AdministratorHeader as Header } from '../Header'
 
 export default function AdministratorMainLayout({ children }: { children: ReactNode }) {
 	return (
 		<>
-			<header>Header</header>
+			<Header />
 			<main>{children}</main>
 		</>
 	)
--- app/(routes)/(main)/administrator/Header.tsx
+++ app/(routes)/(main)/administrator/Header.tsx
@@ -1,8 +1,49 @@
 import Button from '@/app/_components/Button'
-function AdministratorHeader() {
+import Image from 'next/image'
+import BrandLogoFullGreen from '@/public/BrandLogoFullGreen.svg'
+import Link from 'next/link'
+import { ArrowLeftOnRectangleIcon, Bars3Icon } from '@heroicons/react/24/solid'
+
+export const AdministratorHeader = () => {
 	return (
-		<header className='flex w-full items-center justify-between'>
-			<Button>SideBar</Button>
+		<header className='flex h-[60px] w-full items-center justify-between bg-neutral-gray-1 pr-comfortable shadow-63'>
+			<SideBarButton />
+			<Logo />
+			<LogoutButton />
 		</header>
+	)
+}
+
+function SideBarButton() {
+	return (
+		<Button className='flex h-full items-center gap-compact rounded-none border-none bg-primary-580-20 text-body-lg text-primary-625'>
+			<Bars3Icon className='aspect-square h-[20px]' />
+			SideBar
+		</Button>
+	)
+}
+
+function Logo() {
+	return (
+		<Link
+			href={'/'}
+			className='relative  h-full w-[140px]  overflow-hidden  '
+		>
+			<Image
+				src={BrandLogoFullGreen}
+				alt='Welcome to GreenDeco'
+				width={0}
+				height={0}
+				sizes='100vw'
+			/>
+		</Link>
+	)
+}
+
+function LogoutButton() {
+	return (
+		<span className='rounded-[100%] border-[1px] border-neutral-gray-5 bg-neutral-gray-3 p-compact'>
+			<ArrowLeftOnRectangleIcon className='aspect-square h-[24px] text-black' />
+		</span>
 	)
 }

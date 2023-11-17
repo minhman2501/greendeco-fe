547c0661ef9445d9a08664de10ff0ca2b8a09df8
minhman <phminhman2501@gmail.com>
2023-11-17 20:13:11 +0700

    feat: change form layout && implement form to page

--- app/(routes)/(main)/administrator/(main)/product/create/CreateProductForm.tsx
+++ app/(routes)/(main)/administrator/(main)/product/create/CreateProductForm.tsx
@@ -1,4 +1,5 @@
-import { TextField } from '@/app/_components/form'
+'use client'
+import { TextField, Input } from '@/app/_components/form'
 import Button from '@/app/_components/Button'
 import { useForm, SubmitHandler } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
@@ -57,90 +58,102 @@ export default function CreateProductForm() {
 	return (
 		<form
 			// onSubmit={}
-			className='flex w-full flex-col gap-cozy text-body-sm'
+			className='w-full'
 		>
-			<div className='flex-row-between gap-cozy'>
-				<div className='flex-1'>
-					<TextField
-						type='text'
-						label='Product Name'
-						placeholder='The name of the product'
-						register={register('name')}
-						error={Boolean(errors?.name)}
-						helperText={errors?.name?.message}
-					/>
-				</div>
-				<div className='flex-1'>
-					<TextField
-						type='text'
-						label='Brief description of the product'
-						placeholder='Product description must have at least 20 characters'
-						register={register('description')}
-						error={Boolean(errors?.description)}
-						helperText={errors?.description?.message}
-					/>
-				</div>
-			</div>
-			<div>
-				<TextField
-					type='text'
-					label='Type of plant'
-					placeholder=''
-					register={register('type')}
-					error={Boolean(errors?.type)}
-					helperText={errors?.type?.message}
-				/>
-			</div>
-			<div>
-				<TextField
-					type='text'
-					label='Size'
-					placeholder=''
-					register={register('size')}
-					error={Boolean(errors?.size)}
-					helperText={errors?.size?.message}
-				/>
-			</div>
-			<div>
-				<TextField
-					type='text'
-					label='Watering condition'
-					placeholder=''
-					register={register('water')}
-					error={Boolean(errors?.water)}
-					helperText={errors?.water?.message}
-				/>
-			</div>
-			<div>
-				<TextField
-					type='text'
-					label='Caring Difficulty'
-					placeholder=''
-					register={register('difficulty')}
-					error={Boolean(errors?.difficulty)}
-					helperText={errors?.difficulty?.message}
-				/>
-			</div>
+			<div className='grid w-full grid-cols-2 gap-comfortable'>
+				<>
+					<div className='flex-col-start gap-cozy text-body-md'>
+						<div className='flex-1'>
+							<TextField
+								type='text'
+								label='Product Name'
+								placeholder='The name of the product'
+								register={register('name')}
+								error={Boolean(errors?.name)}
+								helperText={errors?.name?.message}
+							/>
+						</div>
+						<div className='flex-1'>
+							<Input
+								multiline
+								placeholder='Product description must have at least 20 characters'
+								className='min-h-[300px]'
+								{...register('description')}
+							/>
+						</div>
+						<div className='flex flex-wrap gap-cozy'>
+							<div>
+								<TextField
+									type='text'
+									label='Type of plant'
+									placeholder=''
+									register={register('type')}
+									error={Boolean(errors?.type)}
+									helperText={errors?.type?.message}
+								/>
+							</div>
+							<div>
+								<TextField
+									type='text'
+									label='Size'
+									placeholder=''
+									register={register('size')}
+									error={Boolean(errors?.size)}
+									helperText={errors?.size?.message}
+								/>
+							</div>
+							<div>
+								<TextField
+									type='text'
+									label='Watering condition'
+									placeholder=''
+									register={register('water')}
+									error={Boolean(errors?.water)}
+									helperText={errors?.water?.message}
+								/>
+							</div>
+							<div>
+								<TextField
+									type='text'
+									label='Caring Difficulty'
+									placeholder=''
+									register={register('difficulty')}
+									error={Boolean(errors?.difficulty)}
+									helperText={errors?.difficulty?.message}
+								/>
+							</div>
 
-			<div>
-				<TextField
-					type='text'
-					label='Light Condition'
-					placeholder=''
-					register={register('light')}
-					error={Boolean(errors?.light)}
-					helperText={errors?.light?.message}
-				/>
-			</div>
-			<div>
-				<TextField
-					type='text'
-					label='Detail of the product'
-					placeholder=''
-					register={register('detail')}
-					error={Boolean(errors?.detail)}
-					helperText={errors?.detail?.message}
-				/>
+							<div>
+								<TextField
+									type='text'
+									label='Light Condition'
+									placeholder=''
+									register={register('light')}
+									error={Boolean(errors?.light)}
+									helperText={errors?.light?.message}
+								/>
+							</div>
+						</div>
+						<div>
+							<TextField
+								type='text'
+								label='Detail of the product'
+								placeholder=''
+								register={register('detail')}
+								error={Boolean(errors?.detail)}
+								helperText={errors?.detail?.message}
+							/>
+						</div>
+					</div>
+				</>
+				<div>
+					<label>Product Images</label>
+					<input
+						type='file'
+						id='upload-photo'
+						accept='image/*'
+					/>
+				</div>
 			</div>
 			<Button
 				type='submit'
--- app/(routes)/(main)/administrator/(main)/product/create/page.tsx
+++ app/(routes)/(main)/administrator/(main)/product/create/page.tsx
@@ -1,10 +1,12 @@
 import Block from '@/app/_components/Block'
+import CreateProductForm from './CreateProductForm'
 
 export default function CreateProductPage() {
 	return (
 		<div className='min-h-screen py-comfortable'>
 			<Block>
 				<h1>Create a new product</h1>
+				<CreateProductForm />
 			</Block>
 		</div>
 	)

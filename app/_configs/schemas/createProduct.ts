7a1fabc8cc854c724753f617b9c6bb515650fd08
minhman <phminhman2501@gmail.com>
2023-11-18 12:00:23 +0700

    feat: remove max character for description and detail

--- app/_configs/schemas/createProduct.ts
+++ app/_configs/schemas/createProduct.ts
@@ -5,12 +5,8 @@ export const CreateProductSchema = z.object({
 	name: z.string().min(1, REQUIRED_FIELD_STRING).max(32, 'Name must be less than 32 characters'),
 	description: z
 		.string()
-		.min(20, 'The detail of the product must contain at least 20 characters')
-		.max(50, 'Detail must be less than 50 characters'),
-	detail: z
-		.string()
-		.min(20, 'The detail of the product must contain at least 20 characters')
-		.max(200, 'Detail must be less than 200 characters'),
+		.min(20, 'The detail of the product must contain at least 20 characters'),
+	detail: z.string().min(20, 'The detail of the product must contain at least 20 characters'),
 	type: z.string().min(1, REQUIRED_FIELD_STRING),
 	size: z.string().min(1, REQUIRED_FIELD_STRING),
 	water: z.string().min(1, REQUIRED_FIELD_STRING),

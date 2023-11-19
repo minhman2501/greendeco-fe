0017a799acf82848f8a41d0ccb4d095e15342bcf
minhman <phminhman2501@gmail.com>
2023-11-19 14:29:35 +0700

    feat: change get product data function and title name

--- app/(routes)/(main)/administrator/(main)/product/[productId]/layout.tsx
+++ app/(routes)/(main)/administrator/(main)/product/[productId]/layout.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Metadata } from 'next'
-import { getProductDetailById } from '@/app/_api/axios/product'
+import { getProductBaseById } from '@/app/_api/axios/product'
 
 export const generateMetadata = async (props: {
 	params: { productId: string }
 }): Promise<Metadata> => {
 	const { params } = props
-	const productName = await getProductDetailById(params.productId).then((res) => res.product.name)
+	const productName = await getProductBaseById(params.productId).then((res) => res.items.name)
 	return {
-		title: productName,
+		title: `${productName} Detail`,
 	}
 }
 export default function ProductDetailLayout({ children }: { children: React.ReactNode }) {

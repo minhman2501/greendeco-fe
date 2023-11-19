4d35db7705dd35b46299dc8d3fe8d124f55b1fbe
minhman <phminhman2501@gmail.com>
2023-11-19 08:49:08 +0700

    feat: variant validation

---
+++ app/_configs/schemas/createVariant.ts
@@ -0,0 +1,33 @@
+import * as z from 'zod'
+import { REQUIRED_FIELD_STRING } from '../constants/variables'
+
+export const CreateVariantSchema = z
+	.object({
+		name: z
+			.string()
+			.min(1, REQUIRED_FIELD_STRING)
+			.max(32, 'Name must be less than 32 characters'),
+		color: z.string().min(1, REQUIRED_FIELD_STRING).max(10, 'HEX color code only'),
+		color_name: z
+			.string()
+			.min(1, REQUIRED_FIELD_STRING)
+			.max(10, 'Color name must be less than 10 character'),
+		description: z
+			.string()
+			.min(20, 'The detail of the product must contain at least 20 characters'),
+		image: z.string().min(1, REQUIRED_FIELD_STRING),
+		is_default: z.boolean(),
+		available: z.boolean(),
+		price: z.string().min(1, REQUIRED_FIELD_STRING),
+	})
+	.refine((data) => isOnlyNumer(data.price), {
+		path: ['price'],
+		message: 'Numbers only',
+	})
+
+function isOnlyNumer(data: string) {
+	const num = Number(data)
+	if (isNaN(num)) return false
+	return true
+}
+export type CreateVariantFormInputType = z.infer<typeof CreateVariantSchema>

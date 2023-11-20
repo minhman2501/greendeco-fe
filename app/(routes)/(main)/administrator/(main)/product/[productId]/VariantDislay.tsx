14e44fc6cbf292430290d1d1a9baebf63d26f7b8
minhman <phminhman2501@gmail.com>
2023-11-20 16:03:13 +0700

    feat: css seleting variant

--- app/(routes)/(main)/administrator/(main)/product/[productId]/VariantDislay.tsx
+++ app/(routes)/(main)/administrator/(main)/product/[productId]/VariantDislay.tsx
@@ -7,16 +7,19 @@ export default function VariantDisplay({ variantList }: { variantList: VariantDa
 	const [currentVariant, setCurrentVariant] = useState<VariantData>(variantList[0])
 	return (
 		<>
-			<div className='flex items-center gap-cozy'>
+			<div className='mb-cozy flex items-center gap-cozy'>
 				<h2>Variants</h2>
 				<ul className='flex divide-x'>
 					{variantList.map((variant) => (
 						<li
 							key={variant.id}
 							onClick={() => setCurrentVariant(variant)}
-							className='px-cozy first:pl-0 last:pr-0'
+							className={clsx(' px-cozy first:pl-0 last:pr-0')}
 						>
-							<VariantListItem {...variant} />
+							<VariantListItem
+								{...variant}
+								active={currentVariant.id === variant.id}
+							/>
 						</li>
 					))}
 				</ul>
@@ -27,20 +30,35 @@ export default function VariantDisplay({ variantList }: { variantList: VariantDa
 }
 
 const VariantListItem = ({
+	active = false,
 	color,
 	color_name,
 }: {
-	id: VariantData['id']
+	active?: boolean
 	color: VariantData['color']
 	color_name: VariantData['color_name']
 }) => {
 	return (
-		<div className={clsx('flex items-center gap-[8px]')}>
+		<div
+			className={clsx('group flex items-center gap-[8px]  ', {
+				'hover:cursor-pointer': !active,
+				'pointer-events-none': active,
+			})}
+		>
 			<span
-				className={clsx('h-[30px] w-[40px] rounded-[4px]')}
+				className={clsx('h-[30px] w-[40px] rounded-[4px]', {
+					'border-[4px] border-primary-625': active,
+				})}
 				style={{ backgroundColor: color }}
 			></span>
-			<p className={clsx('text-body-sm capitalize text-primary-418')}>{color_name}</p>
+			<p
+				className={clsx('text-body-sm capitalize text-primary-418', {
+					'group-hover:font-semi-bold': !active,
+					'font-bold': active,
+				})}
+			>
+				{color_name}
+			</p>
 		</div>
 	)
 }

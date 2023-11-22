817e42b32b9ee97071665d5858c0bf9866135450
minhman <phminhman2501@gmail.com>
2023-11-22 19:57:08 +0700

    feat: implement edit form container

--- app/(routes)/(main)/administrator/(main)/product/edit/[productId]/page.tsx
+++ app/(routes)/(main)/administrator/(main)/product/edit/[productId]/page.tsx
@@ -1,9 +1,9 @@
 'use client'
 import Block from '@/app/_components/Block'
-import ProductEditForm from './ProductEditForm'
 import { useQuery } from '@tanstack/react-query'
 import { getProductBaseById } from '@/app/_api/axios/product'
 import { ADMIN_QUERY_KEY, UseQueryKeys } from '@/app/_configs/constants/queryKey'
+import EditFormContainer from './ProductImageProvider'
 
 export default function EditProductPage({
 	params: { productId },
@@ -22,11 +22,11 @@ export default function EditProductPage({
 	return (
 		<div className='min-h-screen py-comfortable'>
 			<Block>
-				{isSuccess && (
+				{isSuccess && data && (
 					<>
 						<h1>Edit Product</h1>
 						<div className='mt-comfortable border-x-[1px] border-x-primary-625-60 px-comfortable'>
-							<ProductEditForm {...data.items} />
+							<EditFormContainer {...data.items} />
 						</div>
 					</>
 				)}

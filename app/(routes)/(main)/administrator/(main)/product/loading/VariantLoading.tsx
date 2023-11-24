a2891d6d86fa0d30bdbefe3bdf71ca0c72867da7
minhman <phminhman2501@gmail.com>
2023-11-25 01:11:32 +0700

    feat: develop VariantFormLoading

--- app/(routes)/(main)/administrator/(main)/product/loading/VariantLoading.tsx
+++ app/(routes)/(main)/administrator/(main)/product/loading/VariantLoading.tsx
@@ -21,3 +21,50 @@ export function VariantDetailLoading() {
 		</div>
 	)
 }
+
+export function VariantFormLoading() {
+	return (
+		<div className='grid grid-cols-2 gap-comfortable'>
+			<SkeletonTheme
+				baseColor='#e4e8e3'
+				highlightColor='#f7f8f7'
+				duration={2}
+				borderRadius={4}
+			>
+				<div className='flex-col-start gap-comfortable'>
+					<span>
+						<Skeleton
+							height={40}
+							className='mb-comfortable'
+						/>
+						<p>
+							<Skeleton
+								height={16}
+								count={4}
+								className='mb-cozy'
+							/>
+						</p>
+					</span>
+					<span>
+						<Skeleton
+							height={40}
+							className='mb-comfortable'
+						/>
+						<p>
+							<Skeleton
+								height={16}
+								count={4}
+								className='mb-cozy'
+							/>
+						</p>
+					</span>
+				</div>
+				<Skeleton
+					width={'60%'}
+					height={'auto'}
+					className='mb-comfortable aspect-square'
+				/>
+			</SkeletonTheme>
+		</div>
+	)
+}

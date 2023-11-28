9df6f71bfc1e94d36c7c1bba95ce069022276f8e
minhman <phminhman2501@gmail.com>
2023-11-28 21:58:27 +0700

    feat: handle send shipping adress && handle directing after creat order successfully

--- app/(routes)/(main)/checkout/ShippingDetailForm.tsx
+++ app/(routes)/(main)/checkout/ShippingDetailForm.tsx
@@ -5,13 +5,17 @@ import {
 	ShippingAddressSchema,
 	ShippingAddressFormInputType,
 } from '@/app/_configs/schemas/shippingAddress'
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { createOrder } from '@/app/_api/axios/order'
 import { AxiosError } from 'axios'
 import { TextField } from '@/app/_components/form'
 import Button from '@/app/_components/Button'
+import { deleteCookie } from 'cookies-next'
+import { useRouter } from 'next/navigation'
 
 export default function ShippingDetailForm() {
+	const router = useRouter()
+	const queryClient = useQueryClient()
 	const defaultInputValues: ShippingAddressFormInputType = {
 		city: '',
 		district: '',
@@ -32,23 +36,46 @@ export default function ShippingDetailForm() {
 		defaultValues: defaultInputValues,
 	})
 
+	const handleCreateOrderSuccess = () => {
+		reset()
+		deleteCookie('cartId')
+		queryClient.invalidateQueries(['cart'])
+		router.replace('/')
+	}
+
 	const createOrderMutation = useMutation({
 		//NOTE: The callback used for the mutation
 		mutationFn: createOrder,
 		//NOTE: Execuse after receiving suscess responses
-		onSuccess: () => {
-			reset()
-		},
+		onSuccess: handleCreateOrderSuccess,
 		//NOTE: Execuse after receving failure responses
-		/* onError: (e) => {
+		onError: (e) => {
 			if (e instanceof AxiosError) {
+				console.log(e)
 			}
-		}, */
+		},
 	})
+
+	const handleJoiningValues = (values: ShippingAddressFormInputType) => {
+		const { city, ward, district, address } = values
+		const cityWithLabel = city.toLowerCase().includes('city') ? city : `${city} City`
+		const wardWithLabel = ward.toLowerCase().includes('city') ? ward : `${ward} Ward`
+		const districtWithLabel = district.toLowerCase().includes('district')
+			? district
+			: `District ${district}`
+		return `${address}, ${wardWithLabel}, ${districtWithLabel}, ${cityWithLabel}`
+	}
 
 	const onSubmitHandler: SubmitHandler<ShippingAddressFormInputType> = (values, e) => {
 		e?.preventDefault()
 		//NOTE: Execute the Mutation
+		const shippingAddress = handleJoiningValues(values)
+
+		console.log(shippingAddress)
+
+		createOrderMutation.mutate({
+			shipping_address: shippingAddress,
+		})
 	}
 	return (
 		<div className='flex-col-start gap-cozy rounded-[8px] bg-neutral-gray-1 p-comfortable shadow-38'>

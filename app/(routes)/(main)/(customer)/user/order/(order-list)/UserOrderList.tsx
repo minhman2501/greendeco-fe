bf82eb6828bb438b3bb314911b61f542e85d079e
minhman <phminhman2501@gmail.com>
2023-11-30 11:21:22 +0700

    feat: change css

--- app/(routes)/(main)/(customer)/user/order/(order-list)/UserOrderList.tsx
+++ app/(routes)/(main)/(customer)/user/order/(order-list)/UserOrderList.tsx
@@ -1,8 +1,12 @@
 import { OrderData, OrderListData } from '@/app/_api/axios/order'
+import formatDate from '@/app/_hooks/useFormatDate'
+import { DocumentMagnifyingGlassIcon } from '@heroicons/react/24/solid'
+import clsx from 'clsx'
+import Link from 'next/link'
 
 export default function UserOrderList({ orderList }: { orderList: OrderListData['items'] }) {
 	return (
-		<ul className='flex-col-start w-full gap-cozy'>
+		<ul className='flex-col-start w-full gap-compact'>
 			{orderList.map((order) => (
 				<li key={order.id}>
 					<OrderItem order={order} />
@@ -14,16 +18,42 @@ export default function UserOrderList({ orderList }: { orderList: OrderListData[
 
 const OrderItem = ({ order }: { order: OrderData }) => {
 	return (
-		<div className='flex-col-start w-full gap-compact rounded-[4px] bg-neutral-gray-1 p-cozy shadow-38'>
-			<div className='flex items-center justify-between'>
-				<span>
-					Order: <span>{order.id}</span>
+		<div className='flex items-center justify-between rounded-[4px] border-[2px] border-primary-418-20 bg-neutral-gray-1 p-cozy shadow-38'>
+			<div className='flex items-center gap-cozy'>
+				<span className='text-body-md text-primary-5555-80'>
+					Order: <span className='font-semi-bold text-primary-418'>{order.id}</span>
 				</span>
-				<span>{order.state}</span>
 			</div>
-			<div className='flex justify-end'>
-				<span>Detai</span>
-			</div>
+			<span className='flex items-center gap-cozy'>
+				<span className='text-body-xsm text-primary-625-80'>
+					{formatDate(new Date(order.created_at))}
+				</span>
+				<span
+					className={clsx(
+						'rounded-[16px] px-comfortable py-compact text-center text-body-xsm font-semi-bold capitalize',
+						{
+							'bg-order-status-draft text-neutral-gray-1': order.state === 'draft',
+							'bg-order-status-processing text-neutral-gray-1':
+								order.state === 'processing',
+							'bg-order-status-completed text-neutral-gray-1':
+								order.state === 'completed',
+							'bg-order-status-cancelled text-neutral-gray-1':
+								order.state === 'cancelled',
+						},
+					)}
+				>
+					{order.state}
+				</span>
+				<Link
+					href={'/'}
+					className='btn w-fit px-comfortable py-compact'
+				>
+					<span className='flex w-full items-center justify-center gap-[4px] text-body-sm text-neutral-gray-1'>
+						View
+						<DocumentMagnifyingGlassIcon className='aspect-square h-[20px]' />
+					</span>
+				</Link>
+			</span>
 		</div>
 	)
 }

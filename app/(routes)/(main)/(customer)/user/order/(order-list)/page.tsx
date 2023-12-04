93831cc846da3004405b13491013d11796a1e6e5
minhman <phminhman2501@gmail.com>
2023-12-04 15:21:55 +0700

    feat: handle loading, empty data, error

--- app/(routes)/(main)/(customer)/user/order/(order-list)/page.tsx
+++ app/(routes)/(main)/(customer)/user/order/(order-list)/page.tsx
@@ -6,6 +6,8 @@ import useQueryParams from '@/app/_hooks/useQueryParams'
 import { FilterParams } from '@/app/_api/axios/product'
 import UserOrderList from './UserOrderList'
 import OrderListPagination from './OrderPagination'
+import { MutatingDots } from 'react-loader-spinner'
+import { ArchiveBoxXMarkIcon, ExclamationTriangleIcon } from '@heroicons/react/24/solid'
 
 export default function OrderHistoryPage() {
 	const { queryObject } = useQueryParams<FilterParams>()
@@ -22,10 +24,25 @@ export default function OrderHistoryPage() {
 		refetchOnReconnect: true,
 	})
 
-	const { data } = userOrderListQuery
+	const { data, isLoading, isError } = userOrderListQuery
 
 	return (
 		<>
+			{isLoading && (
+				<div className='flex w-full justify-center'>
+					<MutatingDots
+						height='100'
+						width='100'
+						color='#4fa94d'
+						secondaryColor='#4fa94d'
+						radius='12.5'
+						ariaLabel='mutating-dots-loading'
+						wrapperStyle={{}}
+						wrapperClass=''
+						visible={true}
+					/>
+				</div>
+			)}
 			{data && data.page_size > 0 && (
 				<div className='flex-col-start gap-cozy'>
 					<UserOrderList orderList={data.items} />
@@ -36,6 +53,35 @@ export default function OrderHistoryPage() {
 					/>
 				</div>
 			)}
+			{data && data.page_size === 0 && <NoItemFoundMessage />}
+			{isError && <ErrorMessage />}
 		</>
+	)
+}
+
+function NoItemFoundMessage() {
+	return (
+		<div className='flex h-[200px] w-full items-center justify-center text-primary-418'>
+			<span className='flex-col-center gap-compact'>
+				<ArchiveBoxXMarkIcon className='aspect-square h-[80px]' />
+
+				<p className='text-body-md'>Not available</p>
+			</span>
+		</div>
+	)
+}
+
+function ErrorMessage() {
+	return (
+		<div className='flex h-[200px] w-full items-center justify-center text-status-error'>
+			<span className='flex-col-center gap-compact'>
+				<ExclamationTriangleIcon className='aspect-square h-[80px]' />
+
+				<p className='text-body-md font-semi-bold'>
+					Countering errors when trying to get the orders. You can try to re-login your
+					account to fix this problem.
+				</p>
+			</span>
+		</div>
 	)
 }

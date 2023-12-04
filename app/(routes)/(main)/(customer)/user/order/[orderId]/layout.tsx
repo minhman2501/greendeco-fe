f3b77fa10aa9a815011fad3545f8f9e833b0b10e
minhman <phminhman2501@gmail.com>
2023-12-04 15:52:59 +0700

    feat: change metadata

--- app/(routes)/(main)/(customer)/user/order/[orderId]/layout.tsx
+++ app/(routes)/(main)/(customer)/user/order/[orderId]/layout.tsx
@@ -2,8 +2,9 @@ import { ReactNode } from 'react'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-	title: 'Order list',
-	description: 'Where you can view your order list',
+	title: 'Order Detail',
+	description:
+		'Where you can view your order detail and submit your payment if you have not paid',
 }
 
 export default function UserOrderListPageLayout({ children }: { children: ReactNode }) {

7bff9829d1ee3ed1b2ad6ed750e3e3a620557347
minhman <phminhman2501@gmail.com>
2023-12-05 21:18:19 +0700

    feat: change css

--- app/_components/review/index.tsx
+++ app/_components/review/index.tsx
@@ -2,7 +2,7 @@ import ReviewDialog, { ReviewDialogProps } from './ReviewDialog'
 
 export default function ReviewDialogContainer(props: ReviewDialogProps) {
 	return (
-		<div className='sticky left-[50%] top-[50%] z-50 w-fit translate-x-[-50%] translate-y-[-50%]'>
+		<div className='sticky flex h-full items-center justify-center'>
 			<ReviewDialog {...props} />
 		</div>
 	)

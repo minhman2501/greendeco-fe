761024c47b7d46b3003675cb71c959d2da3eb957
minhman <phminhman2501@gmail.com>
2023-12-05 17:06:55 +0700

    feat: change css && add review button

--- app/(routes)/(main)/(customer)/user/reviews/PurchasedProductList.tsx
+++ app/(routes)/(main)/(customer)/user/reviews/PurchasedProductList.tsx
@@ -1,9 +1,12 @@
 import { ProductData } from '@/app/_api/axios/product'
 import Image from 'next/image'
+import Button from '@/app/_components/Button'
+import { StarIcon } from '@heroicons/react/24/outline'
+import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
 
 export default function PurchasedProductList({ productList }: { productList: ProductData[] }) {
 	return (
-		<ul className=' grid grid-cols-2 gap-cozy  '>
+		<ul className=' grid gap-cozy lg:grid-cols-2  '>
 			{productList.map((product) => (
 				<li key={product.id}>
 					<Item product={product} />
@@ -14,7 +17,6 @@ export default function PurchasedProductList({ productList }: { productList: Pro
 }
 
 const Item = ({ product }: { product: ProductData }) => {
-	const { name, images, id, price, currency } = product
 	return (
 		<div className='flex w-full gap-compact overflow-hidden rounded-[16px] border-[1px] border-primary-580-60 bg-neutral-gray-1 shadow-18'>
 			<ItemImage imageSrc={product.images} />
@@ -25,7 +27,7 @@ const Item = ({ product }: { product: ProductData }) => {
 
 function ItemImage({ imageSrc }: { imageSrc: ProductData['images'] }) {
 	return (
-		<div className='relative aspect-square w-[160px] overflow-hidden  '>
+		<div className='relative aspect-square w-[140px] overflow-hidden  '>
 			<Image
 				src={imageSrc[0]}
 				fill
@@ -35,13 +37,25 @@ function ItemImage({ imageSrc }: { imageSrc: ProductData['images'] }) {
 		</div>
 	)
 }
-const ItemDetail = ({ name }: ProductData) => {
+const ItemDetail = ({ name, id }: ProductData) => {
 	return (
-		<div className='flex-col-start h-full justify-between p-cozy'>
-			<div className='flex-col-start gap-[4px]'>
-				<div className='flex items-center justify-between text-primary-625'>
-					<span className='text-body-md font-semi-bold '>{name}</span>
-				</div>
+		<div className='flex-col-start flex-1 justify-between p-cozy '>
+			<span className='text-body-md font-semi-bold text-primary-418'>{name}</span>
+			<div className='flex items-center justify-between gap-cozy'>
+				<Button
+					type='button'
+					className='flex  flex-1 items-center justify-center gap-[4px] text-body-sm font-semi-bold'
+				>
+					Review
+					<StarIcon className='aspect-square h-[16px]' />
+				</Button>
+				<Button
+					type='button'
+					className='btnSecondary flex flex-1 items-center justify-center gap-[4px] text-body-sm font-semi-bold'
+				>
+					View
+					<MagnifyingGlassIcon className='aspect-square h-[16px]' />
+				</Button>
 			</div>
 		</div>
 	)

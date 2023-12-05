e460501af6248a3577cd9e2e3633765705087a8b
minhman <phminhman2501@gmail.com>
2023-12-05 18:24:31 +0700

    feat: add star validaton

--- app/_configs/schemas/review.ts
+++ app/_configs/schemas/review.ts
@@ -1,12 +1,16 @@
-import { MIN_PASSWORD, MAX_PASSWORD } from '../constants/variables'
 import * as z from 'zod'
 
-export const ReviewSchema = z.object({
-	review: z
-		.string()
-		.min(20, 'Your comment needs to have more than 20 characters')
-		.max(100, 'No more than 100 characters'),
-	star: z.string(),
-})
+export const ReviewSchema = z
+	.object({
+		review: z
+			.string()
+			.min(5, 'Your comment needs to have more than 5 characters')
+			.max(40, 'No more than 40 characters'),
+		star: z.string(),
+	})
+	.refine((data) => parseInt(data.star) > 0, {
+		path: ['star'],
+		message: 'Please leave a rating',
+	})
 
 export type ReviewFormInputType = z.infer<typeof ReviewSchema>

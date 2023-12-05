730641081aa26155a80c88d3328c547eb89843c6
minhman <phminhman2501@gmail.com>
2023-12-05 18:09:32 +0700

    feat: change data type

--- app/_configs/schemas/review.ts
+++ app/_configs/schemas/review.ts
@@ -5,9 +5,8 @@ export const ReviewSchema = z.object({
 	review: z
 		.string()
 		.min(20, 'Your comment needs to have more than 20 characters')
-		.max(100, 'No more than 100 characters')
-		.email('Email is invalid'),
-	star: z.number().min(0).max(5, 'You can only rate up to 5 star only'),
+		.max(100, 'No more than 100 characters'),
+	star: z.string(),
 })
 
-export type ReviewInputType = z.infer<typeof ReviewSchema>
+export type ReviewFormInputType = z.infer<typeof ReviewSchema>

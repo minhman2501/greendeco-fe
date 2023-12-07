fc68be5a28b4e9bc72da01622824fcb9d3743221
minhman <phminhman2501@gmail.com>
2023-12-07 09:22:59 +0700

    feat: add user purchased product query

--- app/_configs/constants/queryKey.ts
+++ app/_configs/constants/queryKey.ts
@@ -1,8 +1,18 @@
+import { ORDER_STATE_FIELD } from './variables'
+
 export enum UseQueryKeys {
 	Review = 'review',
 	User = 'user',
 	Product = 'product',
+	Order = 'order',
 	Variant = 'variant',
 }
 
 export const ADMIN_QUERY_KEY = 'admin'
+
+export const USER_PURCHASED_PRODUCTS_QUERY_KEYS = [
+	UseQueryKeys.Product,
+	UseQueryKeys.User,
+	UseQueryKeys.Order,
+	ORDER_STATE_FIELD.completed.state,
+]

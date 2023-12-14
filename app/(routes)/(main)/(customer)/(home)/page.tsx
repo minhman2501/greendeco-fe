b614f6e186541c0cc872c85158b7d506d27f3f94
minhman <phminhman2501@gmail.com>
2023-12-14 14:37:52 +0700

    feat: develop Hero

--- app/(routes)/(main)/(customer)/(home)/page.tsx
+++ app/(routes)/(main)/(customer)/(home)/page.tsx
@@ -1,9 +1,11 @@
 import BuyingProcess from './BuyingProcess'
+import Hero from './Hero'
 import WhyChooseUs from './WhyChooseUs'
 
 export default function CustomerHomePage() {
 	return (
 		<>
+			<Hero />
 			<BuyingProcess />
 			<WhyChooseUs />
 		</>

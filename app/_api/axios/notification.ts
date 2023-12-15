0ccc5b3f044bc7c73df9b5d4b76a71c9ae0c70b2
minhman <phminhman2501@gmail.com>
2023-12-15 16:27:18 +0700

    feat: add filterParams and notification constant querykey

--- app/_api/axios/notification.ts
+++ app/_api/axios/notification.ts
@@ -3,6 +3,7 @@ import { NotificationData } from './admin/notification'
 import { AccessTokenType } from '@/app/_types'
 import { getCookie } from 'cookies-next'
 import { ACCESS_TOKEN_COOKIE_NAME } from '@/app/_configs/constants/cookies'
+import { FilterParams, fieldJSONParse } from './product'
 
 const NOTIFICAION_URL = `${process.env.NEXT_PUBLIC_GREENDECO_BACKEND_API}/notification`
 
@@ -18,7 +19,11 @@ export const notificationApi = axios.create({
 	baseURL: NOTIFICAION_URL,
 })
 
-export const getNotificationFromUser = async () => {
+export const getNotificationFromUser = async (params?: FilterParams) => {
+	let paramAfterJSON
+	if (params) {
+		paramAfterJSON = fieldJSONParse(params)
+	}
 	const accessToken: AccessTokenType = getCookie(ACCESS_TOKEN_COOKIE_NAME)?.toString()
 
 	return await notificationApi
@@ -26,6 +31,7 @@ export const getNotificationFromUser = async () => {
 			headers: {
 				Authorization: `Bearer ${accessToken}`,
 			},
+			params: { ...paramAfterJSON },
 		})
 		.then((res) => res.data)
 }

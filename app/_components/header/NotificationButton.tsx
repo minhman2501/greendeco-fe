9d3216221561883c04cf037d9c0d4b8ba9524cdc
minhman <phminhman2501@gmail.com>
2023-12-15 17:15:22 +0700

    feat: handle render notification

--- app/_components/header/NotificationButton.tsx
+++ app/_components/header/NotificationButton.tsx
@@ -3,12 +3,18 @@
 import { BellIcon } from '@heroicons/react/24/solid'
 import { NotificationItem } from '../notification/NotificationItem'
 import { useState } from 'react'
+import useNotification from '@/app/_hooks/useNotification'
+import NotificationList from '../notification/NotificationList'
 
 export default function NotificationDisplayButton() {
+	const [open, setOpen] = useState(false)
 	const handleCartButtonOnClick = () => {
 		setOpen(!open)
 	}
-	const [open, setOpen] = useState(false)
+
+	const { userNotificationQuery } = useNotification({})
+
+	const { data, isSuccess, isError, isFetching } = userNotificationQuery
 
 	return (
 		<div className='relative'>
@@ -16,6 +22,9 @@ export default function NotificationDisplayButton() {
 				onClick={handleCartButtonOnClick}
 				className='group rounded-xl border-[1px] border-primary-5555-40 bg-primary-5555-20/40 px-[8px] py-[4px] text-primary-625 transition duration-75 ease-in hover:bg-primary-625 hover:text-neutral-gray-1'
 			>
+				{data && data?.page_size > 0 && (
+					<NotificationAmountDisplay amount={data.page_size} />
+				)}
 				<BellIcon className='aspect-square h-[24px] ' />
 			</button>
 			{open && (
@@ -23,21 +32,15 @@ export default function NotificationDisplayButton() {
 					<div className='bg-primary-625-20/40 p-compact py-compact text-heading-3 font-bold text-primary-5555'>
 						Notification
 					</div>
-					<div className='p-compact'>
-						<span className='text-body-sm font-semi-bold text-primary-418-80'>
-							Most Recent
-						</span>
-						<ul className='flex-col-start w-full  '>
-							<NotificationItem />
-						</ul>
-					</div>
+
+					{data && <NotificationList {...data} />}
 				</div>
 			)}
 		</div>
 	)
 }
 
-function NotificationAmountDisplay(amount: number | undefined) {
+function NotificationAmountDisplay({ amount }: { amount: number | undefined }) {
 	if (amount && amount > 0)
 		return (
 			<span className='absolute right-[-4px] top-[-4px] flex aspect-square h-[16px] items-center justify-center rounded-[100%] border-[1px] border-primary-625 bg-primary-625 font-semi-bold text-white transition duration-75  ease-in group-hover:bg-white group-hover:text-primary-625'>

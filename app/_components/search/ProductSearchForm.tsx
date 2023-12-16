5fa9289ff010977c6dc7bd3c87ce4590f84a65a6
minhman <phminhman2501@gmail.com>
2023-12-16 19:11:52 +0700

    feat: add notes

--- app/_components/search/ProductSearchForm.tsx
+++ app/_components/search/ProductSearchForm.tsx
@@ -5,7 +5,7 @@ import { TextField } from '@/app/_components/form'
 import Button from '@/app/_components/Button'
 import { ProductSearchFormInputType, ProductSearchSchema } from '@/app/_configs/schemas/search'
 import useQueryParams from '@/app/_hooks/useQueryParams'
-import { SHOP_ROUTE } from '@/app/_configs/constants/variables'
+import { MIN_SEARCH_VALUE, SHOP_ROUTE } from '@/app/_configs/constants/variables'
 
 type QueryParams = {
 	field: string | null
@@ -42,25 +42,30 @@ export default function ProductSearchForm() {
 			<span className='text-body-md font-semi-bold text-primary-5555'>
 				What do you have in mind?
 			</span>
-			<form
-				onSubmit={handleSubmit(onSubmitHandler)}
-				className='flex w-full items-center gap-cozy text-body-sm'
-			>
-				<TextField
-					className='flex-1'
-					type='text'
-					placeholder='Enter Your Plant'
-					register={register('search')}
-					error={false}
-				/>
-				<Button
-					disabled={!isDirty || isValid === false}
-					type='submit'
-					className='rounded-[8px] px-comfortable font-semi-bold'
+			<div className='flex-col-start w-full gap-[4px]'>
+				<form
+					onSubmit={handleSubmit(onSubmitHandler)}
+					className='flex w-full items-center gap-cozy text-body-sm'
 				>
-					Search
-				</Button>
-			</form>
+					<TextField
+						className='flex-1'
+						type='text'
+						placeholder='Enter Your Plant'
+						register={register('search')}
+						error={false}
+					/>
+					<Button
+						disabled={!isDirty || !isValid}
+						type='submit'
+						className='rounded-[8px] px-comfortable font-semi-bold'
+					>
+						Search
+					</Button>
+				</form>
+				<p className='text-body-xsm italic text-primary-418-60'>
+					* Must contain at least {MIN_SEARCH_VALUE} characters and ONLY characters
+				</p>
+			</div>
 		</div>
 	)
 }

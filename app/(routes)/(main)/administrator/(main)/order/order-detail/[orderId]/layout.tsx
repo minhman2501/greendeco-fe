121219cb0ba10c352a3ee7ab42aea4a9c719db05
minhman <phminhman2501@gmail.com>
2023-12-18 18:06:13 +0700

    feat: change css

--- app/(routes)/(main)/administrator/(main)/order/order-detail/[orderId]/layout.tsx
+++ app/(routes)/(main)/administrator/(main)/order/order-detail/[orderId]/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import type { Metadata } from 'next'
+import Block from '@/app/_components/Block'
 
 export const metadata: Metadata = {
 	title: 'Order Detail',
@@ -8,8 +9,8 @@ export const metadata: Metadata = {
 
 export default function OrderAdminDetailLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<div className='flex-col-start w-full gap-cozy py-comfortable'>
-			<div>{children}</div>
+		<div className='w-full py-comfortable'>
+			<Block>{children}</Block>
 		</div>
 	)
 }

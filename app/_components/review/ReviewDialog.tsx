59919189ae31a669400bd6e7e8def8a101885ebf
minhman <phminhman2501@gmail.com>
2023-12-20 19:50:38 +0700

    feat: implement clickOutside for review dialog

--- app/_components/review/ReviewDialog.tsx
+++ app/_components/review/ReviewDialog.tsx
@@ -1,12 +1,25 @@
 import { ProductData } from '@/app/_api/axios/product'
 import CreateReviewForm from './ReviewForm'
+import { useRef } from 'react'
+import useClickOutside from '@/app/_hooks/useClickOutside'
+import { useDialogStore } from '@/app/_configs/store/useDialogStore'
 
 export type ReviewDialogProps = {
 	productId: ProductData['id']
 }
 export default function ReviewDialog(props: ReviewDialogProps) {
+	const { closeDialog } = useDialogStore()
+	const reviewDialogRef = useRef<any>()
+
+	useClickOutside(reviewDialogRef, () => {
+		closeDialog()
+	})
+
 	return (
-		<div className='flex min-w-[600px] rounded-[16px] bg-neutral-gray-1 p-comfortable'>
+		<div
+			ref={reviewDialogRef}
+			className='flex min-w-[600px] rounded-[16px] bg-neutral-gray-1 p-comfortable'
+		>
 			<CreateReviewForm productId={props.productId} />
 		</div>
 	)

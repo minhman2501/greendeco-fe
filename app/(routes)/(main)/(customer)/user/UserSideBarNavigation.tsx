20fac973a11479aa53bc2fe2226691cfbf9b00c9
minhman <phminhman2501@gmail.com>
2023-12-20 15:23:48 +0700

    feat: remove coupon section

--- app/(routes)/(main)/(customer)/user/UserSideBarNavigation.tsx
+++ app/(routes)/(main)/(customer)/user/UserSideBarNavigation.tsx
@@ -10,6 +10,7 @@ import {
 	HandThumbUpIcon,
 } from '@heroicons/react/24/solid'
 import clsx from 'clsx'
+import useActivePath from '@/app/_hooks/useActivePath'
 
 type UserNavigation = {
 	label: string
@@ -27,11 +28,6 @@ const userNavigationList: UserNavigation[] = [
 		path: '/order',
 		label: 'Order list',
 		icon: <TruckIcon className='aspect-square w-[24px]' />,
-	},
-	{
-		path: '/coupon',
-		label: 'Coupon & Voucher',
-		icon: <TicketIcon className='aspect-square w-[24px]' />,
 	},
 	{
 		path: '/purchased',
@@ -57,17 +53,16 @@ export default React.memo(function UserSidebarNavigation() {
 
 const Navigation = React.memo(function Navigation({ label, path, icon }: UserNavigation) {
 	const rootPath = '/user'
-	const currentPath = usePathname()
-	console.log(path, 'render')
+	const { isPathActive } = useActivePath()
 
 	return (
 		<Link
 			href={rootPath + path}
 			className={clsx('flex w-full items-center  gap-compact rounded-l-[8px] p-cozy ', {
 				'cursor-pointer text-primary-625 hover:font-semibold':
-					currentPath.includes(path) === false,
+					isPathActive(`${rootPath}${path}`) === false,
 				'pointer-events-none cursor-auto bg-primary-625 font-bold text-neutral-gray-1':
-					currentPath.includes(path),
+					isPathActive(`${rootPath}${path}`) === true,
 			})}
 		>
 			{icon}

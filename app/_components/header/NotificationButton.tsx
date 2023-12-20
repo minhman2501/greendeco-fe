ffb4a794456ae12f21076b73b1fdb4fcff899d4b
minhman <phminhman2501@gmail.com>
2023-12-20 17:28:53 +0700

    feat: implement useClickOutside for notification

--- app/_components/header/NotificationButton.tsx
+++ app/_components/header/NotificationButton.tsx
@@ -1,22 +1,26 @@
 'use client'
 
 import { BellIcon } from '@heroicons/react/24/solid'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import useNotification from '@/app/_hooks/useNotification'
 import NotificationList from '../notification/NotificationList'
 import { motion, useScroll, useMotionValueEvent, AnimatePresence } from 'framer-motion'
+import useClickOutside from '@/app/_hooks/useClickOutside'
 
 export default function NotificationDisplayButton() {
 	const [open, setOpen] = useState(false)
+	const notificationListRef = useRef<any>()
 
-	const { scrollY } = useScroll()
-
-	const handleOpenNotification = () => {
+	const handleToggleNotification = () => {
 		setOpen(!open)
 	}
+	useClickOutside(notificationListRef, () => {
+		if (open) {
+			setOpen(false)
+		}
+	})
 
-	const { userNotificationQuery } = useNotification({})
-
+	const { scrollY } = useScroll()
 	useMotionValueEvent(scrollY, 'change', (latestWindowY) => {
 		const previousWindowY = scrollY.getPrevious()
 		if (latestWindowY > previousWindowY && latestWindowY > 90) {
@@ -24,12 +28,16 @@ export default function NotificationDisplayButton() {
 		}
 	})
 
+	const { userNotificationQuery } = useNotification({})
 	const { data, isSuccess, isError } = userNotificationQuery
 
 	return (
-		<div className='relative'>
+		<div
+			ref={notificationListRef}
+			className='relative'
+		>
 			<button
-				onClick={handleOpenNotification}
+				onClick={handleToggleNotification}
 				className='group rounded-xl border-[1px] border-primary-5555-40 bg-primary-5555-20/40 px-[8px] py-[4px] text-primary-625 transition duration-75 ease-in hover:bg-primary-625 hover:text-neutral-gray-1'
 			>
 				{data && data?.page_size > 0 && (
@@ -37,29 +45,31 @@ export default function NotificationDisplayButton() {
 				)}
 				<BellIcon className='aspect-square h-[24px] ' />
 			</button>
-			<AnimatePresence>
-				{open && (
-					<motion.div
-						initial={{
-							translateY: '-16px',
-							opacity: 0,
-						}}
-						animate={{ opacity: 1, translateY: 0 }}
-						exit={{
-							opacity: 0,
-							translateY: '-16px',
-						}}
-						transition={{ ease: 'easeInOut', duration: 0.2 }}
-						className='absolute right-0 top-[calc(100%+8px)] max-h-[60vh] w-[24vw] overflow-y-auto rounded-xl bg-white shadow-38 '
-					>
-						<div className='bg-primary-625-20/40 p-compact text-heading-3 font-bold text-primary-5555'>
-							Notification
-						</div>
+			<div>
+				<AnimatePresence>
+					{open && (
+						<motion.div
+							initial={{
+								translateY: '-16px',
+								opacity: 0,
+							}}
+							animate={{ opacity: 1, translateY: 0 }}
+							exit={{
+								opacity: 0,
+								translateY: '-16px',
+							}}
+							transition={{ ease: 'easeInOut', duration: 0.2 }}
+							className='absolute right-0 top-[calc(100%+8px)] max-h-[60vh] w-[24vw] overflow-y-auto rounded-xl bg-white shadow-38 '
+						>
+							<div className='bg-primary-625-20/40 p-compact text-heading-3 font-bold text-primary-5555'>
+								Notification
+							</div>
 
-						{data && <NotificationList {...data} />}
-					</motion.div>
-				)}
-			</AnimatePresence>
+							{data && <NotificationList {...data} />}
+						</motion.div>
+					)}
+				</AnimatePresence>
+			</div>
 		</div>
 	)
 }
